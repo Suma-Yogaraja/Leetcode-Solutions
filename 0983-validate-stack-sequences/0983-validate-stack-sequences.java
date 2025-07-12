d217e956c0869b6a4ea0429699b2b65ce692a15a
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-07-11 17:22:27 -0700

    Time: 34 ms (1.64%), Space: 44.9 MB (6.61%) - LeetHub

---
+++ 0983-validate-stack-sequences/0983-validate-stack-sequences.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> stack = new Stack<>();
+      
+        int j=0;
+        for(int num:pushed){
+            //first push the number to stack
+                stack.push(num);
+                System.out.println(num);
+        //now check whether that pushed num is same as pop ,if same pop that elemnt
+            while(!stack.isEmpty()&& j<popped.length && stack.peek()==popped[j]){
+                
+                stack.pop();
+                j++;
+                
+               // System.out.println("stack peek" + stack.peek());
+               // System.out.println(j);
+            }
+        }
+        return stack.isEmpty();
+    }
+}

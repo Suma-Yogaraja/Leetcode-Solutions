532339eccee1c6f53f5726aba554db7daef5a628
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-07-14 10:37:09 -0700

    Time: 51 ms (5.8%), Space: 45.4 MB (53.67%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+        
+
+        for(int i:asteroids){
+            boolean alive=true;
+            while(!stack.isEmpty() && i<0 && stack.peek()>0){
+                if((stack.peek()+i)<0){
+                    stack.pop();
+                    continue;
+                }
+                else if((stack.peek())== Math.abs(i)){
+                    stack.pop();
+                }
+                alive=false;
+                break;
+
+            }
+            if(alive){
+            stack.push(i);
+            System.out.println(i);
+            }
+
+        }
+       
+        int[] result = new int[stack.size()];
+         for (int j = stack.size()- 1; j >= 0; j--) {
+            result[j] = stack.pop(); // modifies the stack (empties it)
+        }
+        return result;
+    }
+}

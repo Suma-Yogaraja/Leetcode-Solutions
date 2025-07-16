c59076ecf1b35ba57b72e57588ccdb3640d3fd14
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-07-15 18:07:11 -0700

    Time: 5 ms (75.33%), Space: 58.3 MB (22.64%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxArea(int[] height) {
+        //the problem is similar to finding the rectangle that has highest area
+        //for highesh area,length and breath should be max
+        //here length is value of array,width is index of array
+        int length=0;
+        int width=0;
+        int last=height.length-1;
+        int first=0;
+        int maxArea=Integer.MIN_VALUE;
+        while(first<=last){
+        
+            length=Math.min(height[first],height[last]);
+            width=last-first;
+            maxArea=Math.max(maxArea,length*width);
+            if(height[last]<=height[first])
+                last--;
+            else 
+                first++;
+
+        }
+
+        return maxArea;
+    }
+}

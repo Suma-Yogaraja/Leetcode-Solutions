634618ee107c734420bb41f09153cfad26daec64
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-07-15 18:09:25 -0700

    Time: 5 ms (75.33%), Space: 58.2 MB (31.78%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -4,16 +4,16 @@ class Solution {
         //for highesh area,length and breath should be max
         //here length is value of array,width is index of array
         int length=0;
-        int width=0;
+        //int width=0;
         int last=height.length-1;
         int first=0;
         int maxArea=Integer.MIN_VALUE;
         while(first<=last){
         
             length=Math.min(height[first],height[last]);
-            width=last-first;
-            maxArea=Math.max(maxArea,length*width);
-            if(height[last]<=height[first])
+            //width=last-first;
+            maxArea=Math.max(maxArea,length*(last-first));
+            if(first<=last && height[last]<=height[first])
                 last--;
             else 
                 first++;

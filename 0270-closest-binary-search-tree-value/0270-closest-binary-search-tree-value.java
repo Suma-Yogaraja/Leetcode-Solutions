28eae09457c28fa74f67fc3afa8ab1f5173473d3
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-07-19 13:38:13 -0700

    Time: 0 ms (100%), Space: 43.4 MB (22.99%) - LeetHub

--- 0270-closest-binary-search-tree-value/0270-closest-binary-search-tree-value.java
+++ 0270-closest-binary-search-tree-value/0270-closest-binary-search-tree-value.java
@@ -19,36 +19,65 @@ class Solution {
     double minDiff;
 
     public int closestValue(TreeNode root, double target) {
+        //since its BST,we can make use of that propert to optimise the search
         if (root == null)
             return 0;
         targetSum = target;
         closest = root.val;
         minDiff = Math.abs(target - root.val);
-        //System.out.println(minDiff);
         dfs(root);
         return closest;
-
     }
 
     void dfs(TreeNode node){
         if(node==null)
-         return;
-        //take node  value and check aganist minDiff value 
-      double currDiff=Math.abs(targetSum-node.val);
-      //System.out.println(currDiff);
-      if(currDiff<minDiff){
-       minDiff=currDiff;
-       closest=node.val;   
-       //System.out.println(closest);
-      }
-      if(currDiff==minDiff)
-        {
-            if(closest>node.val)
-              closest=node.val;
+            return;
+        double currDiff=Math.abs(node.val-targetSum);
+        if(currDiff<minDiff){
+            minDiff=currDiff;
+            closest=node.val;
         }
-        //do recursive
-        dfs(node.left);
-        dfs(node.right);
+        if(currDiff==minDiff && closest>node.val)
+            closest=node.val;
+
+        if(targetSum>node.val)
+            dfs(node.right);
         
+        else
+            dfs(node.left);
+
     }
 }
+
+//     if (root == null)
+//         return 0;
+//     targetSum = target;
+//     closest = root.val;
+//     minDiff = Math.abs(target - root.val);
+//     //System.out.println(minDiff);
+//     dfs(root);
+//     return closest;
+
+// }
+
+// void dfs(TreeNode node){
+//     if(node==null)
+//      return;
+//     //take node  value and check aganist minDiff value 
+//   double currDiff=Math.abs(targetSum-node.val);
+//   //System.out.println(currDiff);
+//   if(currDiff<minDiff){
+//    minDiff=currDiff;
+//    closest=node.val;   
+//    //System.out.println(closest);
+//   }
+//   if(currDiff==minDiff)
+//     {
+//         if(closest>node.val)
+//           closest=node.val;
+//     }
+//     //do recursive
+//     dfs(node.left);
+//     dfs(node.right);
+
+// }

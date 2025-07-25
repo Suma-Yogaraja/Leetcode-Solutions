ced38b7dbddd232e2731aa927c0bf6c7e7134ac8
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-07-25 12:54:08 -0700

    Time: 106 ms (40%), Space: 127.4 MB (8.19%) - LeetHub

--- 2445-reachable-nodes-with-restrictions/2445-reachable-nodes-with-restrictions.java
+++ 2445-reachable-nodes-with-restrictions/2445-reachable-nodes-with-restrictions.java
@@ -4,28 +4,29 @@ class Solution {
     public int reachableNodes(int n, int[][] edges, int[] restricted) {
         //first convert grid array to adj 
         restrict = new HashSet<>();
-        int reach=0;
         Map<Integer, List<Integer>> graph = new HashMap<>();
         boolean[] visited = new boolean[n];
+        int reach =0;
         for (int i = 0; i < n; i++) {
             graph.put(i, new ArrayList<>());
         }
         for (int edge[] : edges) {
-            graph.get(edge[0]).add(edge[1]);
-            graph.get(edge[1]).add(edge[0]);
+            int from = edge[0];
+            int to = edge[1];
+            graph.get(from).add(to);
+            graph.get(to).add(from);
         }
         //initilaize the set
         for (int i : restricted)
             restrict.add(i);
         //start with node 0,call ddfs
-         reach= reach+dfs(0, graph, visited);
+        reach += dfs(0, graph, visited);
         return reach;
     }
 
     int dfs(int node, Map<Integer, List<Integer>> graph, boolean[] visited) {
         //dfs check its neighbour node and check if its not in restricted set and not visited before
         int reach = 1;
-        
         visited[node] = true;
         for (int neighbour : graph.get(node)) {
             if (!visited[neighbour] && !restrict.contains(neighbour)) {

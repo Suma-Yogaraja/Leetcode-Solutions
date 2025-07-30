0a56147e85b5295052e5740de52adcac7600aade
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-07-30 11:43:12 -0700

    Time: 2 ms (96.89%), Space: 43.4 MB (63.87%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -5,22 +5,43 @@ class Solution {
         //sort both the array and take tow pointers,check if elemnt at i=i-1,if so duplicate is present
         //soincrement the pointer ,same holds for other array
         Set<Integer> num1=new HashSet<>();
-        Set<Integer> num2=new HashSet<>();
-        //int k=0;
-        List<Integer> result=new ArrayList<>();
+        Set<Integer> result=new HashSet<>();
         for(int i:nums1){
             num1.add(i);
         }
         for(int i:nums2){
-            num2.add(i);
-        }
-        for(int j:num1){
-            if(num2.contains(j)){
-                result.add(j);
+            if(num1.contains(i)){
+                result.add(i);
             }
         }
-        int[] res=result.stream().mapToInt(Integer::intValue).toArray();
-
+        int[] res=new int[result.size()];
+        int k=0;
+        for(int j:result){
+            res[k]=j;
+            k++;
+        }
         return res;
     }
 }
+
+
+
+
+
+
+
+
+
+//         for(int i:nums2){
+//             num2.add(i);
+//         }
+//         for(int j:num1){
+//             if(num2.contains(j)){
+//                 result.add(j);
+//             }
+//         }
+//         int[] res=result.stream().mapToInt(Integer::intValue).toArray();
+
+//         return res;
+//     }
+// }

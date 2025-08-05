5fa4435ec0096078dd1ea66fbfd1e6fddfda12d5
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-08-05 12:36:30 -0700

    Time: 2 ms (84.3%), Space: 52.1 MB (93.98%) - LeetHub

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.java
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.java
@@ -14,21 +14,59 @@
  * }
  */
 class Solution {
+    int goodNode;
     public int goodNodes(TreeNode root) {
-        //we use DFS to traverse the tree
-        return dfs(root,Integer.MIN_VALUE);
+        goodNode=0;
+    //List<TreeNode> good=new ArrayList<>();
+    int maxValue=Integer.MIN_VALUE;
+
+    if(root==null)
+        return 0;
+    dfs(root,maxValue);
+    return goodNode;
     }
-    //write a function that return the total number of good nodes
-    int dfs(TreeNode node,int max){
-        
+    void dfs(TreeNode node,int maxValue){
         if(node==null)
-            return 0;
-        int left=dfs(node.left,Math.max(max,node.val));
-        int right=dfs(node.right,Math.max(max,node.val));
-        int ans=left+right;
-        if(node.val>=max)
-            ans++;
-        return ans;
+            return;
+        if(node.val>=maxValue){
+            maxValue=node.val;
+            goodNode++;
+            //System.out.println(node.val);
+        }
+        dfs(node.left,maxValue);
+        dfs(node.right,maxValue);
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//         //we use DFS to traverse the tree
+//         return dfs(root,Integer.MIN_VALUE);
+//     }
+//     //write a function that return the total number of good nodes
+//     int dfs(TreeNode node,int max){
+        
+//         if(node==null)
+//             return 0;
+//         int left=dfs(node.left,Math.max(max,node.val));
+//         int right=dfs(node.right,Math.max(max,node.val));
+//         int ans=left+right;
+//         if(node.val>=max)
+//             ans++;
+//         return ans;
+
+//     }
+// }

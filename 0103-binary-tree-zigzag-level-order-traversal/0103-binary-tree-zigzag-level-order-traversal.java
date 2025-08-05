1294998ff5070a167532576e40523771d07048b5
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-08-05 13:18:07 -0700

    Time: 1 ms (63.86%), Space: 42.8 MB (7.19%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -15,33 +15,74 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> innerList = new ArrayList<>();
-        Queue<TreeNode> q = new LinkedList<TreeNode>();
-        boolean dir = true;
-        int depth = 0;//depth even,from right else from left
-        if (root == null)
-            return ans;
-        q.add(root);
-        while (!q.isEmpty()) {
-            int size = q.size();
-            innerList = new ArrayList<>();
-            for (int i = 0; i < size; i++) {
-                TreeNode node = q.remove();
+        //bfs
+        if(root==null)
+            return new ArrayList<>();// List<List<Integer>>
+        Queue<TreeNode> q=new LinkedList<>();
+        List<List<Integer>> res=new ArrayList<>();
+        boolean dir=true;//true is considered as left to right dir
+        q.offer(root);
+        while(!q.isEmpty()){
+            int size=q.size();
+            List<Integer> innerList=new ArrayList<>();
+            for(int i=0;i<size;i++){
+                TreeNode node=q.poll();
                 innerList.add(node.val);
-                if (node.left != null)
-                    q.add(node.left);
-                if (node.right != null)
-                    q.add(node.right);
+                    if(node.left!=null)
+                        q.offer(node.left);
+                    if(node.right!=null)
+                        q.offer(node.right);     
             }
-            if (dir)
-                ans.add(innerList);
-            else {
+            if(dir)
+                res.add(innerList);
+            else{
                 Collections.reverse(innerList);
-                ans.add(innerList);
+                res.add(innerList);
             }
-            dir = !dir;
+            dir=!dir;
         }
-        return ans;
+    return res;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+//         List<List<Integer>> ans = new ArrayList<>();
+//         List<Integer> innerList = new ArrayList<>();
+//         Queue<TreeNode> q = new LinkedList<TreeNode>();
+//         boolean dir = true;
+//         int depth = 0;//depth even,from right else from left
+//         if (root == null)
+//             return ans;
+//         q.add(root);
+//         while (!q.isEmpty()) {
+//             int size = q.size();
+//             innerList = new ArrayList<>();
+//             for (int i = 0; i < size; i++) {
+//                 TreeNode node = q.remove();
+//                 innerList.add(node.val);
+//                 if (node.left != null)
+//                     q.add(node.left);
+//                 if (node.right != null)
+//                     q.add(node.right);
+//             }
+//             if (dir)
+//                 ans.add(innerList);
+//             else {
+//                 Collections.reverse(innerList);
+//                 ans.add(innerList);
+//             }
+//             dir = !dir;
+//         }
+//         return ans;
+//     }
+// }

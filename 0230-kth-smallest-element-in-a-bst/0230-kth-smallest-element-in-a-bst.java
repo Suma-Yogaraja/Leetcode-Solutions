21a23c47f92207cbf6bd1d1d2a9b19e8e7265a7e
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-08-05 13:43:34 -0700

    Time: 2 ms (8.97%), Space: 44.4 MB (75.29%) - LeetHub

---
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        //here as tree is BST ,we take advantage of the property that if it is travered in inorder,we get ascending order
+        //create an array of int in ascending order
+        //return kth elemnt in that array
+        List<Integer> res=new ArrayList<>();
+        inOrder(root,res);
+        int[] ans=new int[res.size()];
+        for(int i=0;i<res.size();i++){
+            ans[i]=res.get(i);
+        }
+        return ans[--k];
+    }
+    void inOrder(TreeNode node,List<Integer> res){
+        if(node==null)
+            return;
+        inOrder(node.left,res);
+        res.add(node.val);
+        inOrder(node.right,res);
+    }
+}

aaf53b4aa5b4e1ea9b928dc54b3fc817fc2ee457
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-08-14 12:40:39 -0700

    Time: 3 ms (21.6%), Space: 42.5 MB (5.78%) - LeetHub

---
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int minMutation(String startGene, String endGene, String[] bank) {
+        //similar to word ladder,here we use bfs and treat the problem as graph
+        Queue<String> q=new LinkedList<>();
+        Set<String> bankSet=new HashSet<>(Arrays.asList(bank));
+        Set<String> visited=new HashSet<>();
+        int steps=0;
+        // if(!bankSet.contains(endGene))
+        //     return -1;
+        q.add(startGene);
+        visited.add(startGene);
+        while(!q.isEmpty()){
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                String cell=q.poll();
+                if(cell.equals(endGene))
+                    return steps;
+                //change a word in the string check if its there in bank
+                for(int j=0;j<cell.length();j++){
+                    for(char c='A';c<='Z';c++){
+                        String word=replace(cell,c,j);
+                        //System.out.println(word);
+                        if(!bankSet.contains(word) || visited.contains(word))
+                            continue;    
+                        visited.add(word);
+                        q.offer(word);
+                    }
+                }
+            }
+            steps++;
+        }
+        return -1;
+    }
+    private String replace(String cell,char changeTo,int index){
+        char[] chars=cell.toCharArray();
+        chars[index]=changeTo;
+        return new String(chars);
+    }
+}

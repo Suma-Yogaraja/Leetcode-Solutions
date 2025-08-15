07ada0990af9e41e3b871ab4faa8df1cb496b088
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-08-15 15:12:08 -0700

    Time: 7 ms (34.36%), Space: 44.8 MB (43.66%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,27 +1,45 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        Set<Character> seen = new HashSet<>();
-        //use two pointer sliding window concept 
-        int f = 0;
-        int l = 0;
-        int maxLen = 0;
-
-        while (l < s.length()) {
-
-            while (seen.contains(s.charAt(l))) {
-                //remove the left character and increment 
-                seen.remove(s.charAt(f));
-                f++;
+        int first = 0;
+        int last = 0;
+        int maxLength=0;
+        Set<Character> set = new HashSet<>();
+        while (last < s.length()) {
+            while (set.contains(s.charAt(last))) {
+                set.remove(s.charAt(first));
+                first++;
             }
-            seen.add(s.charAt(l));
-            maxLen = Math.max(maxLen, seen.size());
-            l++;
+                set.add(s.charAt(last));
+                maxLength=Math.max(maxLength,set.size());
+               // System.out.println(maxLength);
+                last++;
         }
-
-        return maxLen;
-
+        return maxLength;
     }
 }
+
+//         Set<Character> seen = new HashSet<>();
+//         //use two pointer sliding window concept 
+//         int f = 0;
+//         int l = 0;
+//         int maxLen = 0;
+
+//         while (l < s.length()) {
+
+//             while (seen.contains(s.charAt(l))) {
+//                 //remove the left character and increment 
+//                 seen.remove(s.charAt(f));
+//                 f++;
+//             }
+//             seen.add(s.charAt(l));
+//             maxLen = Math.max(maxLen, seen.size());
+//             l++;
+//         }
+
+//         return maxLen;
+
+//     }
+// }
 
 //         Set<Character> seen=new HashSet<>();
 //         int l=0,r=0,maxlen=0;

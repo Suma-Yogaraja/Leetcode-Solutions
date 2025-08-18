3f8c01af9800999648d5224c2379b4bbf497725d
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-08-18 15:12:28 -0700

    Time: 199 ms (74.72%), Space: 58.6 MB (29.31%) - LeetHub

---
+++ 2310-minimum-operations-to-halve-array-sum/2310-minimum-operations-to-halve-array-sum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int halveArray(int[] nums) {
+        double target=0;
+        PriorityQueue<Double> heap=new PriorityQueue<>(Comparator.reverseOrder());
+        for(double num:nums){
+            target+=num;
+            heap.add(num);
+        }
+            target/=2;
+            int operation=0;
+            while(target>0){
+                operation++;
+                double x=heap.remove();
+                target-=(x/2);
+                heap.add(x/2);
+            }
+            return operation;
+    }
+}

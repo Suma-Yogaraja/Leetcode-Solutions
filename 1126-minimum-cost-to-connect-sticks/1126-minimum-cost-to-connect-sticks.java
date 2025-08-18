e6f2166b707ee226c92cb8c6da43624b8278e945
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-08-18 15:47:31 -0700

    Time: 61 ms (94.7%), Space: 45.4 MB (36.93%) - LeetHub

--- 1126-minimum-cost-to-connect-sticks/1126-minimum-cost-to-connect-sticks.java
+++ 1126-minimum-cost-to-connect-sticks/1126-minimum-cost-to-connect-sticks.java
@@ -1,14 +1,18 @@
 class Solution {
     public int connectSticks(int[] sticks) {
-        PriorityQueue<Integer> heap=new PriorityQueue<>();
-        for(int stick:sticks)
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        if (sticks == null || sticks.length <= 1) {
+            return 0;
+        }
+
+        for (int stick : sticks)
             heap.add(stick);
-        int total=0;
-        while(heap.size()>1){
-            int first=heap.poll();
-            int second=heap.poll();
-            total+=first+second;
-            heap.offer(first+second);
+        int total = 0;
+        while (heap.size() > 1) {
+            int first = heap.poll();
+            int second = heap.poll();
+            total += first + second;
+            heap.offer(first + second);
         }
         return total;
     }

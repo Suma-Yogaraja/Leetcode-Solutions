89d14fa6522a210670d3559b34673edd7a298b6e
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-08-26 09:41:16 -0700

    Time: 2 ms (68.73%), Space: 41.8 MB (23.89%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int sPointer=0;
+        if(s.length()==0)
+            return true;
+        if(t.length()==0)
+            return false;
+        //int pPointer=0;
+        for(int i=0;i<t.length()&&sPointer<s.length() ;i++){
+            char ch=s.charAt(sPointer);
+            if(t.charAt(i)==ch){
+                if(sPointer<s.length())
+                   sPointer++;
+                // else
+                //  return true; 
+            }
+        }
+        return sPointer==s.length()?true: false;
+    }
+}

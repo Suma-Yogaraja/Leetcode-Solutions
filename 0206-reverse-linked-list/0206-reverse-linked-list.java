0754cf23b7e9d2addb0fb1084ea68215fd8bb8a5
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-09-05 12:10:31 -0700

    Time: 0 ms (100%), Space: 42.8 MB (19.14%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -10,18 +10,54 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        //check edge case where head is null
         if(head==null || head.next==null)
             return head;
-        // have three ponter prev current and nextnode
         ListNode prev=null;
         ListNode curr=head;
-        while(curr!=null){
-            ListNode nextNode=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=nextNode;
+        //ListNode nextNode=head.next;
+        while(curr!=null ){
+           ListNode nextNode=curr.next;
+           curr.next=prev;
+           prev=curr;
+           curr=nextNode; 
+
         }
-        return prev;
+      return prev;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//         //check edge case where head is null
+//         if(head==null || head.next==null)
+//             return head;
+//         // have three ponter prev current and nextnode
+//         ListNode prev=null;
+//         ListNode curr=head;
+//         while(curr!=null){
+//             ListNode nextNode=curr.next;
+//             curr.next=prev;
+//             prev=curr;
+//             curr=nextNode;
+//         }
+//         return prev;
+//     }
+// }

7a658e07b5032ac28d4d5e2558a1e3683af8c199
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-09-08 12:47:52 -0700

    Time: 0 ms (100%), Space: 41.8 MB (45.47%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -10,24 +10,72 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        //declare fast and slow pointer and move fast pointer such that it is at a gap of n
-       ListNode dummy=new ListNode(-1);
-       dummy.next=head;
-       if(head==null || head.next==null)
-        return null;
-        ListNode slow=dummy;
-        ListNode fast=dummy;
-        for(int i=0;i<=n;i++){
+        if(head==null)
+            return head;
+        if(head.next==null && n==1)
+            return null;
+        //im making use of tow pointer + sliding window,create a window of size n then move
+        ListNode slow=head;
+        ListNode fast=head;
+        for(int i=0;i<n;i++)
             fast=fast.next;
-        }
-        while(fast!=null){
-            fast=fast.next;
+         if(fast==null)
+            return slow.next;
+        while(fast!=null && fast.next!=null){
             slow=slow.next;
+            fast=fast.next;
         }
+        // System.out.println(slow.val);
+        // System.out.println(fast.val); 
+        
+
         slow.next=slow.next.next;
-        return dummy.next;
+        return head;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//         //declare fast and slow pointer and move fast pointer such that it is at a gap of n
+//        ListNode dummy=new ListNode(-1);
+//        dummy.next=head;
+//        if(head==null || head.next==null)
+//         return null;
+//         ListNode slow=dummy;
+//         ListNode fast=dummy;
+//         for(int i=0;i<=n;i++){
+//             fast=fast.next;
+//         }
+//         while(fast!=null){
+//             fast=fast.next;
+//             slow=slow.next;
+//         }
+//         slow.next=slow.next.next;
+//         return dummy.next;
         
 
      
-    }
-}
+//     }
+// }

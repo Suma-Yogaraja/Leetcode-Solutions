9279cf7a57d2faf15adbed9cc2f0c490cf1da9de
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-09-12 12:23:52 -0700

    Time: 5 ms (63.88%), Space: 45 MB (95.82%) - LeetHub

---
+++ 1192-divide-chocolate/1192-divide-chocolate.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int maximizeSweetness(int[] sweetness, int k) {
+       // Arrays.sort(sweetness);
+        int left=0,right=0;
+        for(int sweet :sweetness){
+            left=Math.min(left,sweet);
+            right+=sweet;
+        }
+        int ans=right;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(isValidCut(sweetness,k,mid)){
+                ans=mid;
+                right=mid-1;
+            }
+            else
+                left=mid+1;
+        }
+        return ans;
+    }
+    private boolean isValidCut(int[] sweetness,int cuts,int maxSweet){
+        int currCut=0;
+        int currSweetness=0;
+        for(int sweet:sweetness){
+            if(currSweetness+sweet>maxSweet){
+                currCut++;
+                currSweetness=0;
+                if(currCut>cuts)
+                    return false;
+            }
+            else
+                currSweetness+=sweet;
+        }
+        return true;
+    }
+}

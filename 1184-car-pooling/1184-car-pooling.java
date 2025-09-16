2b82c15fb3a1fdd420d9e11c1eb8f21bffc841e8
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-09-16 11:35:58 -0700

    Time: 1 ms (100%), Space: 44.5 MB (36.25%) - LeetHub

--- 1184-car-pooling/1184-car-pooling.java
+++ 1184-car-pooling/1184-car-pooling.java
@@ -6,7 +6,7 @@ class Solution {
         int[] arr=new int[far+1];
         for(int i=0;i<trips.length;i++){
             int value=trips[i][0],from=trips[i][1],to=trips[i][2];
-            System.out.println(from + ": " + to + " :" + value);
+            //System.out.println(from + ": " + to + " :" + value);
             arr[from]+=value;
             arr[to]-=value;
         }

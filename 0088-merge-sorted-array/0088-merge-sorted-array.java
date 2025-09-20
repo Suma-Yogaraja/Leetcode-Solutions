22dbc847248423bc295d0acdcf5b27edeab36577
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-09-19 17:48:27 -0700

    Time: 16 ms (2.1%), Space: 43 MB (8.59%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,34 +1,90 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        //use two pointer pointing to last indes of both array
-        int last1=m-1;
-        int last2=n-1;
-        int ansLast=nums1.length-1;
-        //edge cases
+
+        //edge cases,m and n both empty ,either m or n is empty
         if(n==0)
             return;
-        
+        //two pointer
+        int ptr1=m-1;
+        int ptr2=n-1;
+        int idx=nums1.length-1;
+        System.out.println("index" + idx);
+        System.out.println(ptr1+ " " + ptr2);
+        while(ptr1>=0 && ptr2>=0){
+            System.out.println(ptr1 + " : " + ptr2);
+            if(nums1[ptr1]>=nums2[ptr2]){
+                nums1[idx]=nums1[ptr1--];
+                
+            }
+            else{
+                nums1[idx]=nums2[ptr2--];   
+            }
+            idx--;
+        }
 
-        while(m>0 && n>0){
-            //check last elemnts of both array and place at last 
-            if(nums2[n-1]>nums1[m-1]){
-                nums1[ansLast]=nums2[n-1];
-                n--;    
-            }
-            else {
-                nums1[ansLast]=nums1[m-1];
-                m--;
-            }
-            
-            ansLast--;
+        while(ptr1>0){
+            nums1[idx]=nums1[ptr1--];
+            idx--;
         }
-        while(n>0){
-            nums1[ansLast]=nums2[n-1];
-            n--;
-            ansLast--;
+        while(ptr2>=0){
+            nums1[idx]=nums2[ptr2--];
+            idx--;
         }
-            // for(int i:nums1)
-            //     System.out.print(i + ",");
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//         //use two pointer pointing to last indes of both array
+//         int last1=m-1;
+//         int last2=n-1;
+//         int ansLast=nums1.length-1;
+//         //edge cases
+//         if(n==0)
+//             return;
+        
+
+//         while(m>0 && n>0){
+//             //check last elemnts of both array and place at last 
+//             if(nums2[n-1]>nums1[m-1]){
+//                 nums1[ansLast]=nums2[n-1];
+//                 n--;    
+//             }
+//             else {
+//                 nums1[ansLast]=nums1[m-1];
+//                 m--;
+//             }
+            
+//             ansLast--;
+//         }
+//         while(n>0){
+//             nums1[ansLast]=nums2[n-1];
+//             n--;
+//             ansLast--;
+//         }
+//             // for(int i:nums1)
+//             //     System.out.print(i + ",");
+
+//     }
+// }

9eff93194c1a161dfdc0392a19c4ec9038bb25e2
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-09-19 19:05:08 -0700

    Time: 13 ms (28.9%), Space: 49.3 MB (93.73%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,19 +1,45 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        //use hash map,nums as key and freq count as value
-        //iterate through map and check for highest value
-        Map<Integer,Integer> map=new HashMap<>();
-        int maxi=0;
-        int result=0;
-        for(int i:nums){
-            map.put(i,map.getOrDefault(i,0)+1);
+        Map<Integer,Integer> freq=new HashMap<>();
+        for(int num:nums)
+            freq.put(num,freq.getOrDefault(num,0)+1);
+        int limit=nums.length/2;
+       // System.out.println(limit);
+    
+        for(int key:freq.keySet()){
+            if(freq.get(key)>limit)
+               return key;
         }
-        for(int key:map.keySet()){
-            if(map.get(key)>maxi){
-                maxi=map.get(key);
-                result=key;
-            }
-        }
-        return result;
+       return -1; 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//         //use hash map,nums as key and freq count as value
+//         //iterate through map and check for highest value
+//         Map<Integer,Integer> map=new HashMap<>();
+//         int maxi=0;
+//         int result=0;
+//         for(int i:nums){
+//             map.put(i,map.getOrDefault(i,0)+1);
+//         }
+//         for(int key:map.keySet()){
+//             if(map.get(key)>maxi){
+//                 maxi=map.get(key);
+//                 result=key;
+//             }
+//         }
+//         return result;
+//     }
+// }

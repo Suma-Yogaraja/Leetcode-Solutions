ab1eed54b551e67b70807275ee94412d2eb52459
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-09-19 18:09:34 -0700

    Time: 0 ms (100%), Space: 42.2 MB (31.15%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,20 +1,55 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int n=nums.length;
-        int count=0;
-        for(int i=0;i<n;){
-            if(nums[i]==val){
-                count++;
-                for(int j=i;j<n-1;j++){
-                    nums[j]=nums[j+1];
-                }
-                n--;    
+        int start=0;
+       // int count=0;
+        int end=nums.length-1;
+        int temp=0;
+        while(start<=end){
+            if(nums[start]==val){
+                //System.out.println(start + " :" + end);
+                //count++;
+                nums[start]=-1;
+                //swap with last elemnt
+                temp=nums[start];
+                nums[start]=nums[end];
+                nums[end]=temp;
+                //System.out.println("start and end is " + nums[start] + " :" + nums[end]);
+                //start++;
+                end--;
             }
             else{
-                i++;
+                start++;
             }
-                
         }
-        return n;
+        return start;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+//         int n=nums.length;
+//         int count=0;
+//         for(int i=0;i<n;){
+//             if(nums[i]==val){
+//                 count++;
+//                 for(int j=i;j<n-1;j++){
+//                     nums[j]=nums[j+1];
+//                 }
+//                 n--;    
+//             }
+//             else{
+//                 i++;
+//             }
+                
+//         }
+//         return n;
+//     }
+// }

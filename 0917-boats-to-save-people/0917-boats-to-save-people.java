72d2fcf0fc031bd125b92e145f931c81e180758b
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-09-22 18:12:53 -0700

    Time: 16 ms (94.88%), Space: 55.4 MB (19.83%) - LeetHub

---
+++ 0917-boats-to-save-people/0917-boats-to-save-people.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        //two pointer
+        int start=0;
+        int end=people.length-1;
+        int boat=0;
+        Arrays.sort(people);
+        while(start<=end){
+            if((people[start]+people[end])<=limit){
+                start++;
+            }
+            end--;
+            boat++;
+        }
+        return boat;
+    }
+}

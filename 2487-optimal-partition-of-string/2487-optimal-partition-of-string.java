1118470a38b89ee9053b262d00e3b71cffb9bd31
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-09-24 11:26:58 -0700

    Time: 31 ms (62.88%), Space: 45.8 MB (23.43%) - LeetHub

---
+++ 2487-optimal-partition-of-string/2487-optimal-partition-of-string.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int partitionString(String s) {
+        //question ask for substring with unique character so i can use set 
+        Set<Character> set=new HashSet<>();
+        int substringNum=1;;
+        for(char c:s.toCharArray()){
+            if(set.contains(c)){
+                //we can count a new substring from here
+                set.clear();//clear all elemnts from set
+                set.add(c);//append this as new element
+                substringNum++;
+            }
+            else{
+                set.add(c);
+            }
+        }
+        return substringNum;
+    }
+}

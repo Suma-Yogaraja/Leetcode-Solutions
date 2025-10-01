3ba555dc8ac6765dbadb0899d76080b4954937f6
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-10-01 12:15:36 -0700

    Time: 2 ms (92.85%), Space: 47.6 MB (11.55%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,27 +1,51 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-
-            int start=0,end=numbers.length-1;
-            
-
-            while(start<=end){
-
-                //int mid= start=(end-start)/2;
-                int sum=numbers[start]+ numbers[end];
-
-                if(sum>target)
-                 end=end-1;
-                if(sum<target)
-                 start=start+1;
-                if(sum==target)
-                 return new int[] {start+1,end+1};
-
-            }
-
-
-       return new int[] {-1,-1}; 
+        //two pointer
+        int start=0;
+        int end=numbers.length-1;
+        while(start<end){
+            if((numbers[start]+numbers[end])>target)
+                end--;
+            else if((numbers[start]+numbers[end]<target))
+                start++;
+            else
+                return new int[]{start+1,end+1};
+        }
+        return new int[]{-1,-1};
     }
-    
-
-    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//             int start=0,end=numbers.length-1;
+//             while(start<=end){
+//                 //int mid= start=(end-start)/2;
+//                 int sum=numbers[start]+ numbers[end];
+//                 if(sum>target)
+//                  end=end-1;
+//                 if(sum<target)
+//                  start=start+1;
+//                 if(sum==target)
+//                  return new int[] {start+1,end+1};
+//             }
+//        return new int[] {-1,-1}; 
+//     }  
+// }

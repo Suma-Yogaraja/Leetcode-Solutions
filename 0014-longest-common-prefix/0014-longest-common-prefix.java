7cbbf6508b1d6e5603935bb71dd4827026d7c353
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-10-02 13:00:35 -0700

    Time: 1 ms (59.37%), Space: 41.5 MB (63.21%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -6,7 +6,7 @@ class Solution {
             return strs[0];
         for (int i = 0; i < strs[0].length(); i++) {
             char c = strs[0].charAt(i);
-            System.out.println(c);
+            // System.out.println(c);
             for(int j=1;j<strs.length;j++){
                 if(i>=strs[j].length() || c!=strs[j].charAt(i))
                     return strs[0].substring(0,i);

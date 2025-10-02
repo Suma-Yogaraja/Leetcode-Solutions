a0ba45e7083a210addba9bb9644fcd7e3553b50b
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-10-02 15:21:07 -0700

    Time: 1 ms (62.96%), Space: 41.4 MB (5.4%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,31 +1,75 @@
 class Solution {
     public boolean isHappy(int n) {
-        int sum=0;
         if(n==1)
             return true;
-        // if(n>1 && n<=9){
-        //     System.out.println(n);
-        //     return false;
-        // }
-        int digit=digitsCount(n,sum);
-        // System.out.println(digit);
-        if(digit==1 || digit ==7)
-            return true;
-        else
-            return false;
-        
+        int sum=0;
+        Set<Integer> set=new HashSet<>();
+        while(n!=1){
+           n= sumofSquares(n);
+            if(set.contains(n)){//there is a cycle
+                return false;
+            }
+            set.add(n);
+        }
+        return true;
     }
-    private int digitsCount(int n,int sum){
-        
-        int count=0;
+    private int sumofSquares(int n){
+        int sum=0;
         while(n>0){
             sum+=(n%10)*(n%10);
             n=n/10;
-            count++;
-        }
-        while(sum>9){
-           sum= digitsCount(sum,0);
         }
         return sum;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//         int sum=0;
+//         if(n==1)
+//             return true;
+//         // if(n>1 && n<=9){
+//         //     System.out.println(n);
+//         //     return false;
+//         // }
+//         int digit=digitsCount(n,sum);
+//         // System.out.println(digit);
+//         if(digit==1 || digit ==7)
+//             return true;
+//         else
+//             return false;
+        
+//     }
+//     private int digitsCount(int n,int sum){
+        
+//         int count=0;
+//         while(n>0){
+//             sum+=(n%10)*(n%10);
+//             n=n/10;
+//             count++;
+//         }
+//         while(sum>9){
+//            sum= digitsCount(sum,0);
+//         }
+//         return sum;
+//     }
+// }

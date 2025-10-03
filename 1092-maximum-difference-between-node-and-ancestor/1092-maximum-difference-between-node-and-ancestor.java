29cd9d3c4f822c1aab7a20df2be9d174af890210
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-10-03 13:12:50 -0700

    Time: 0 ms (100%), Space: 42.4 MB (25.79%) - LeetHub

--- 1092-maximum-difference-between-node-and-ancestor/1092-maximum-difference-between-node-and-ancestor.java
+++ 1092-maximum-difference-between-node-and-ancestor/1092-maximum-difference-between-node-and-ancestor.java
@@ -16,41 +16,69 @@
 class Solution {
     int maxDiff;
     public int maxAncestorDiff(TreeNode root) {
-        maxDiff=0;
+        maxDiff=Integer.MIN_VALUE;
         if(root==null)
             return 0;
-        dfs(root);
-        return maxDiff;
+        dfs(root,root.val,root.val);
+       return maxDiff;
     }
-    int[] dfs(TreeNode node){
+    //at each node i will send max and min
+    private void dfs(TreeNode node,int min,int max){
         if(node==null)
-            return new int[]{-1,-1};
-        int min=Integer.MAX_VALUE;
-        int max=Integer.MIN_VALUE;
-        if(node.left==null && node.right==null){
-            return new int[] {node.val,node.val};
-        }
-        int[] left=dfs(node.left);
-        int[] right=dfs(node.right);
-        for(int i=0;i<2;i++){
-            if(left[i]!=-1){
-                maxDiff=Math.max(maxDiff,Math.abs(node.val-left[i]));
-                min=Math.min(min,left[i]);
-                max=Math.max(max,left[i]);
-            }
-        }
-         for(int i=0;i<2;i++){
-            if(right[i]!=-1){
-                maxDiff=Math.max(maxDiff,Math.abs(node.val-right[i]));
-                min=Math.min(min,right[i]);
-                max=Math.max(max,right[i]);
-            }
-        }
-        min=Math.min(min,node.val);
+            return ;//for null node
         max=Math.max(max,node.val);
-
-        return new int[] {min,max};
-
+        min=Math.min(min,node.val);
+       int currDiff=max-min;
+        maxDiff=Math.max(maxDiff,currDiff);
+        dfs(node.left,min,max);
+        dfs(node.right,min,max);
+        
     }
-
 }
+
+
+
+
+
+
+
+
+
+//         maxDiff=0;
+//         if(root==null)
+//             return 0;
+//         dfs(root);
+//         return maxDiff;
+//     }
+//     int[] dfs(TreeNode node){
+//         if(node==null)
+//             return new int[]{-1,-1};
+//         int min=Integer.MAX_VALUE;
+//         int max=Integer.MIN_VALUE;
+//         if(node.left==null && node.right==null){
+//             return new int[] {node.val,node.val};
+//         }
+//         int[] left=dfs(node.left);
+//         int[] right=dfs(node.right);
+//         for(int i=0;i<2;i++){
+//             if(left[i]!=-1){
+//                 maxDiff=Math.max(maxDiff,Math.abs(node.val-left[i]));
+//                 min=Math.min(min,left[i]);
+//                 max=Math.max(max,left[i]);
+//             }
+//         }
+//          for(int i=0;i<2;i++){
+//             if(right[i]!=-1){
+//                 maxDiff=Math.max(maxDiff,Math.abs(node.val-right[i]));
+//                 min=Math.min(min,right[i]);
+//                 max=Math.max(max,right[i]);
+//             }
+//         }
+//         min=Math.min(min,node.val);
+//         max=Math.max(max,node.val);
+
+//         return new int[] {min,max};
+
+//     }
+
+// }

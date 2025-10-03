4152f79d41c8672ac9f0183826bd5361dc418b51
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-10-03 16:34:06 -0700

    Time: 20 ms (10.98%), Space: 43.2 MB (86.35%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.java
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -14,28 +14,66 @@
  * }
  */
 class Solution {
-    int ans;
+    
     public int pathSum(TreeNode root, int targetSum) {
-         ans=0;
-         traverse(root,targetSum);
-         return ans;
-    }
-    private void traverse(TreeNode root, int targetSum){
-         if(root==null)
-            return;
-        dfs(root,targetSum,0L);
-        traverse(root.left,targetSum);
-        traverse(root.right,targetSum);
-        
-    }
-    private void dfs(TreeNode root,int target,long curr){
         if(root==null)
-            return;
-        curr+=root.val;
-        if(curr==target)
+            return 0;
+        int ans=dfs(root,targetSum,0L);
+        int left=pathSum(root.left,targetSum);
+        int right=pathSum(root.right,targetSum);
+        return ans+left+right;
+    }
+    public int dfs(TreeNode node,int targetSum,long currVal){
+        int ans=0;
+        if(node==null)
+            return 0;
+        currVal+=node.val;
+        if(currVal==targetSum)
             ans++;
-        dfs(root.left,target,curr);
-        dfs(root.right,target,curr);
- 
+        ans+=dfs(node.left,targetSum,currVal);
+        ans+=dfs(node.right,targetSum,currVal);
+        return ans;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // int ans;
+    // public int pathSum(TreeNode root, int targetSum) {
+    //      ans=0;
+    //      traverse(root,targetSum);
+    //      return ans;
+    // }
+    // private void traverse(TreeNode root, int targetSum){
+//          if(root==null)
+//             return;
+//         dfs(root,targetSum,0L);
+//         traverse(root.left,targetSum);
+//         traverse(root.right,targetSum);
+        
+//     }
+//     private void dfs(TreeNode root,int target,long curr){
+//         if(root==null)
+//             return;
+//         curr+=root.val;
+//         if(curr==target)
+//             ans++;
+//         dfs(root.left,target,curr);
+//         dfs(root.right,target,curr);
+ 
+//     }
+// }

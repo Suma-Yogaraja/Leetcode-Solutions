7c3f23299ad8a0c49ca23e7a752cc7dbfdd1b860
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-10-03 12:24:13 -0700

    Time: 7 ms (15.15%), Space: 63 MB (89.44%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -14,24 +14,56 @@
  * }
  */
 class Solution {
+    int minDepth;
     public int minDepth(TreeNode root) {
-        int depth=0;
-        int mini=0;
-        //use dfs
+        //dfs
         if(root==null)
             return 0;
+        minDepth=Integer.MAX_VALUE;
+       return dfs(root);
+    }
+    private int dfs(TreeNode node){
+        int currDepth=0;
+        if(node==null)
+            return 0; 
+        int leftDepth=dfs(node.left);
+        int rightDepth=dfs(node.right);
+        if(node.left==null)
+            return rightDepth+1;
+        if(node.right==null)
+            return leftDepth+1;
+        if(node.left==null && node.right==null)
+            return currDepth;
         
-        //recursive call
-            int left=minDepth(root.left);
-            int right=minDepth(root.right);
-            if(left==0)
-                return right+1;
-            else if(right==0)
-                return left+1;
-                
-            
-           return Math.min(left,right)+1;
-
-        
+        currDepth=Math.min(leftDepth,rightDepth)+1;
+        return currDepth;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//         int depth=0;
+//         int mini=0;
+//         //use dfs
+//         if(root==null)
+//             return 0;      
+//         //recursive call
+//             int left=minDepth(root.left);
+//             int right=minDepth(root.right);
+//             if(left==0)
+//                 return right+1;
+//             else if(right==0)
+//                 return left+1;
+//            return Math.min(left,right)+1;  
+//     }
+// }

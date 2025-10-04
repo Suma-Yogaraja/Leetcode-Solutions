74953990fb23555409e74ca403cd53c6266f83d6
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-10-03 19:09:32 -0700

    Time: 0 ms (100%), Space: 41.5 MB (31.3%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -14,20 +14,49 @@
  * }
  */
 class Solution {
+
     public int sumNumbers(TreeNode root) {
-      if(root==null)
-        return 0;
-       return dfs(root,0);  
+        if(root==null)
+            return 0;
+        return dfs(root,0);
     }
-    int dfs(TreeNode node,int sum){
+    private int dfs(TreeNode node,int sum){
         if(node==null)
             return 0;
-        sum=sum*10 +node.val;
-        if(node.left==null && node.right==null){
+        sum=sum*10+node.val;
+        if(node.left==null && node.right==null){//leaf node
             return sum;
         }
         return dfs(node.left,sum)+dfs(node.right,sum);
-
-
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//       if(root==null)
+//         return 0;
+//        return dfs(root,0);  
+//     }
+//     int dfs(TreeNode node,int sum){
+//         if(node==null)
+//             return 0;
+//         sum=sum*10 +node.val;
+//         if(node.left==null && node.right==null){
+//             return sum;
+//         }
+//         return dfs(node.left,sum)+dfs(node.right,sum);
+
+
+//     }
+// }

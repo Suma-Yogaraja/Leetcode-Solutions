5c2140fd5a86ce0157991f54244ab645ddae130c
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-10-08 12:56:19 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,18 +1,62 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        //prefix sum [1,2,3]: count[0:1,1:3]
-        int currSum=0;
-        int count=0;
-        Map<Integer,Integer> prefix=new HashMap<>();
-        prefix.put(0,1);
-        for(int num:nums){
-            currSum+=num;
-            count+=prefix.getOrDefault(currSum-k,0);
-            prefix.put(currSum,prefix.getOrDefault(currSum,0)+1);
+        if(nums.length==0)
+            return 0;
+        //Prefix+hashMap
+        Map<Integer,Integer> freq=new HashMap<>();
+        int[] prefix=new int[nums.length];
+        int i=0;
+        int ans=0;
+        prefix[0]=nums[0];
+        for(i=1;i<nums.length;i++){
+            prefix[i]=prefix[i-1]+nums[i];
         }
-        return count;
+        freq.put(0,0);
+        for(i=0;i<nums.length;i++){
+            freq.put(prefix[i],freq.getOrDefault(prefix[i],0)+1);
+            if(freq.containsKey(prefix[i]-k)){
+                ans++;
+            }
+            
+        }
+        return ans;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//         //prefix sum [1,2,3]: count[0:1,1:3]
+//         int currSum=0;
+//         int count=0;
+//         Map<Integer,Integer> prefix=new HashMap<>();
+//         prefix.put(0,1);
+//         for(int num:nums){
+//             currSum+=num;
+//             count+=prefix.getOrDefault(currSum-k,0);
+//             prefix.put(currSum,prefix.getOrDefault(currSum,0)+1);
+//         }
+//         return count;
+//     }
+// }
 
 
 

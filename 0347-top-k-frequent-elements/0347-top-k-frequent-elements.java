4edcbe4649fa53482ada24e12c22ad2d910735e7
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-10-08 11:35:38 -0700

    Time: 15 ms (37.37%), Space: 48.8 MB (43.93%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,21 +1,21 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        //create a hashmap,keep elements in key and its fre as values
-        Map<Integer, Integer> fre = new HashMap<>();
-        for (int i : nums)
-            fre.put(i, fre.getOrDefault(i, 0) + 1);
-        //store it in a heap,freq as first
-        PriorityQueue<Integer> heap=new PriorityQueue<>((n1,n2)-> fre.get(n1)-fre.get(n2));
-        for(int num:fre.keySet()){
-            heap.add(num);
-            if(heap.size()>k)
-                heap.poll();
+        int[] ans ;
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
         }
-        int[] ans=new int[heap.size()];
-        for(int i=0;i<k;i++){
-                ans[i]=heap.poll();
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->freq.get(a)-freq.get(b));
+        for(int key:freq.keySet()){
+            pq.add(key);
+            if(pq.size()>k){
+                pq.remove();
+            }
+        }
+        ans=new int[pq.size()];
+        for(int i=0;i<ans.length;i++){
+            ans[i]=pq.remove();
         }
         return ans;
-       
     }
 }

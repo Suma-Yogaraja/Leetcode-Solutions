40aa5a00d9fcefddef404fc736f168e36b705b14
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-10-13 12:16:08 -0700

    Time: 66 ms (6.22%), Space: 45.6 MB (10.24%) - LeetHub

--- 1126-minimum-cost-to-connect-sticks/1126-minimum-cost-to-connect-sticks.java
+++ 1126-minimum-cost-to-connect-sticks/1126-minimum-cost-to-connect-sticks.java
@@ -1,19 +1,66 @@
 class Solution {
     public int connectSticks(int[] sticks) {
-        PriorityQueue<Integer> heap = new PriorityQueue<>();
-        if (sticks == null || sticks.length <= 1) {
+        List<Integer> ans=new ArrayList<>();
+        if(sticks.length<=1)
             return 0;
+        PriorityQueue<Integer>pq=new PriorityQueue<Integer>();//min heap
+        for(int stick:sticks)
+            pq.add(stick);
+        while(pq.size()>1){
+            int stick1=pq.poll();
+            int stick2=pq.poll();
+            pq.add(stick1+stick2);
+            ans.add(stick1+stick2);    
         }
-
-        for (int stick : sticks)
-            heap.add(stick);
-        int total = 0;
-        while (heap.size() > 1) {
-            int first = heap.poll();
-            int second = heap.poll();
-            total += first + second;
-            heap.offer(first + second);
-        }
-        return total;
+        int res=0;
+        while(!ans.isEmpty())
+            res+=ans.remove(ans.size()-1);
+        return res;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//         PriorityQueue<Integer> heap = new PriorityQueue<>();
+//         if (sticks == null || sticks.length <= 1) {
+//             return 0;
+//         }
+
+//         for (int stick : sticks)
+//             heap.add(stick);
+//         int total = 0;
+//         while (heap.size() > 1) {
+//             int first = heap.poll();
+//             int second = heap.poll();
+//             total += first + second;
+//             heap.offer(first + second);
+//         }
+//         return total;
+//     }
+// }

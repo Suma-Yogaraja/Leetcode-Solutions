7d86eca0c01407c6239e41f322b4f379352ce4bb
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-10-17 13:28:53 -0700

    Time: 34 ms (44.75%), Space: 57.9 MB (35.33%) - LeetHub

--- 2387-partition-array-such-that-maximum-difference-is-k/2387-partition-array-such-that-maximum-difference-is-k.java
+++ 2387-partition-array-such-that-maximum-difference-is-k/2387-partition-array-such-that-maximum-difference-is-k.java
@@ -4,18 +4,14 @@ class Solution {
         //to get min diff,max and min in that subsequence should not be apart so thinking of sorting or heap
         if(nums.length<=1)
             return 1;
-
         Arrays.sort(nums);
         int subSeq=1;
         int min=nums[0];
         int max=nums[0];
         for(int i=1;i<nums.length;i++){
-            min=Math.min(min,nums[i]);
-            max=Math.max(max,nums[i]);
-            int diff=max-min;
+            int diff=nums[i]-min;
             if(diff>k){
                 min=nums[i];
-                max=nums[i];
                 subSeq++;
             }
         }

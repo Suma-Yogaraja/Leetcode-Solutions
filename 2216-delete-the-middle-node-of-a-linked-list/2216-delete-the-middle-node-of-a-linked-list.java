6f53a703e7d373774dd006ec5397892912a11594
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-10-17 13:41:46 -0700

    Time: 3 ms (99.52%), Space: 64 MB (54.58%) - LeetHub

--- 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.java
+++ 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.java
@@ -10,25 +10,72 @@
  */
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
-        //find the length n of the given linked list
-        ListNode dummy=head;
-        int n=0;
-        if(head==null || head.next==null){
-            return null;
+        //i will use fast and slow pointer to get middle of linked list and prevnode to get prev node of slow
+        if(head==null || head.next==null)
+         return null;
+        ListNode node=new ListNode();
+        node.next=head;
+        ListNode fast=head;
+        ListNode slow=head;
+        ListNode prevNode=null;
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            prevNode=slow;
+            slow=slow.next;
         }
-        while(dummy!=null){
-            n++;
-            dummy=dummy.next;
-        }
-       // System.out.println(n);
-       
-        dummy=head;
-        //iterate till n/2 and delink the middle elemnt
-        for(int i=1;i<n/2;i++){
-            dummy=dummy.next;
-        }
-        //System.out.println(dummy.val);
-        dummy.next=dummy.next.next;
-        return head;
+
+       prevNode.next=slow.next;
+        return node.next;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//         //find the length n of the given linked list
+//         ListNode dummy=head;
+//         int n=0;
+//         if(head==null || head.next==null){
+//             return null;
+//         }
+//         while(dummy!=null){
+//             n++;
+//             dummy=dummy.next;
+//         }
+//        // System.out.println(n);
+       
+//         dummy=head;
+//         //iterate till n/2 and delink the middle elemnt
+//         for(int i=1;i<n/2;i++){
+//             dummy=dummy.next;
+//         }
+//         //System.out.println(dummy.val);
+//         dummy.next=dummy.next.next;
+//         return head;
+//     }
+// }

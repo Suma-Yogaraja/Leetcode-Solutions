377ae12131a90eb7b918befc3ffcb3b10ffb47f0
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-10-17 13:16:34 -0700

    Time: 7 ms (50.92%), Space: 45.8 MB (98.92%) - LeetHub

--- 1254-deepest-leaves-sum/1254-deepest-leaves-sum.java
+++ 1254-deepest-leaves-sum/1254-deepest-leaves-sum.java
@@ -17,22 +17,74 @@ class Solution {
     public int deepestLeavesSum(TreeNode root) {
         if(root==null)
             return 0;
-        Queue<TreeNode> q=new LinkedList<TreeNode>();
-        int sum=0;
-        q.add(root);
+        //i will do bfs traversal,level by level order to find last level
+        int currLevelSum=0;
+        Queue<TreeNode> q=new LinkedList<>();
+        q.offer(root);
         while(!q.isEmpty()){
+            currLevelSum=0;
             int size=q.size();
-            sum=0;
             for(int i=0;i<size;i++){
-                TreeNode node=q.remove();
-                sum=sum+node.val;
+                TreeNode node=q.poll();
+                currLevelSum+=node.val;
                 if(node.left!=null)
-                    q.add(node.left);
+                    q.offer(node.left);
                 if(node.right!=null)
-                    q.add(node.right);
+                    q.offer(node.right);
             }
-
         }
-        return sum;
+        return currLevelSum;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//         if(root==null)
+//             return 0;
+//         Queue<TreeNode> q=new LinkedList<TreeNode>();
+//         int sum=0;
+//         q.add(root);
+//         while(!q.isEmpty()){
+//             int size=q.size();
+//             sum=0;
+//             for(int i=0;i<size;i++){
+//                 TreeNode node=q.remove();
+//                 sum=sum+node.val;
+//                 if(node.left!=null)
+//                     q.add(node.left);
+//                 if(node.right!=null)
+//                     q.add(node.right);
+//             }
+
+//         }
+//         return sum;
+//     }
+// }

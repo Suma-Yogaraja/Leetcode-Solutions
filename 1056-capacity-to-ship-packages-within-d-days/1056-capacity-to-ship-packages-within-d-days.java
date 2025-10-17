f8bf678030a3682e1f18703de232175a36a3e6b6
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-10-17 11:20:07 -0700

    Time: 11 ms (76.83%), Space: 47.4 MB (48.61%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
@@ -1,40 +1,94 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-        //find min capacity per day 
-
-        int left=0 ;
-        int right =0;
-        for(int weight:weights){
-            left=Math.max(left,weight);//worst case where capacity is min
-            right+=weight;// best case ship all in one day
-        }
+//this is more of binary search where the question ask for min capacity within d days  
+ //the ans lies in th boundary ->ex1->max=10 so that is the min capacity,max capacity is adding and and shipping in one day
+        int left=0;
+        int right=0;
         int ans=right;
+        for(int weight:weights){
+        left=Math.max(left,weight);
+        right+=weight;
+     } 
+    //ex-1-> ans lies between 55 and 10
         while(left<=right){
-            int mid=left+(right-left)/2;
-            if(canShip(weights,days,mid)){
-                right=mid-1;
+           int mid=left+(right-left)/2;
+           if(isValidCapacity(mid,days,weights)){
+            //if mid valid then,with min capacity of mid,we can send all weights so we will check if mid-1 is also possible
                 ans=mid;
+                right=mid-1;
+                //  System.out.println("im possible so numbers below me is also possible " + right);
             }
-            else
+            else{
                 left=mid+1;
-           // System.out.println("left :right" + left + " : " +right);
+            }
         }
         return ans;
     }
-    private boolean canShip(int[] weights,int totalDay,int maxCap){
+    private boolean isValidCapacity(int max,int days,int[] weights){
+        int currW=0;
         int day=1;
-        int curWeight=0;
-        for(int w:weights){
-            if(curWeight+w>maxCap){
+        for(int weight:weights){
+            if(currW+weight>max){
+                currW=weight;
                 day++;
-                curWeight=w;
-                if(day>totalDay)
+                if(day>days)
                     return false;
             }
-            else
-                curWeight+=w;
-           // System.out.println("curr weight" + curWeight);     
+            else{
+                currW+=weight;
+            }
         }
         return true;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//         //find min capacity per day 
+
+//         int left=0 ;
+//         int right =0;
+//         for(int weight:weights){
+//             left=Math.max(left,weight);//worst case where capacity is min
+//             right+=weight;// best case ship all in one day
+//         }
+//         int ans=right;
+//         while(left<=right){
+//             int mid=left+(right-left)/2;
+//             if(canShip(weights,days,mid)){
+//                 right=mid-1;
+//                 ans=mid;
+//             }
+//             else
+//                 left=mid+1;
+//            // System.out.println("left :right" + left + " : " +right);
+//         }
+//         return ans;
+//     }
+//     private boolean canShip(int[] weights,int totalDay,int maxCap){
+//         int day=1;
+//         int curWeight=0;
+//         for(int w:weights){
+//             if(curWeight+w>maxCap){
+//                 day++;
+//                 curWeight=w;
+//                 if(day>totalDay)
+//                     return false;
+//             }
+//             else
+//                 curWeight+=w;
+//            // System.out.println("curr weight" + curWeight);     
+//         }
+//         return true;
+//     }
+// }

453f814d671fd0bb48cd1c78cb6dde486954a6f2
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-10-17 11:49:08 -0700

    Time: 2 ms (82.8%), Space: 59.5 MB (20.1%) - LeetHub

--- 1428-jump-game-iii/1428-jump-game-iii.java
+++ 1428-jump-game-iii/1428-jump-game-iii.java
@@ -1,29 +1,69 @@
 class Solution {
     public boolean canReach(int[] arr, int start) {
-        Queue<Integer> q=new LinkedList<>();
-        q.offer(start);
-        Set<Integer> visited=new HashSet<>();
-        visited.add(start);
-        while(!q.isEmpty()){
-            int size=q.size();
-            for(int i=0;i<size;i++){
-                int k=q.poll();
-                if(arr[k]==0)
-                    return true;
-                //check up directions is within boundry and not visited
-                int up=k+arr[k];
-                if(up<arr.length && !visited.contains(up) && up>=0){
-                    visited.add(up);
-                    q.offer(up);
-                }
-                 //check down directions is within boundry and not visited
-                int down=k-arr[k];
-                if(down<arr.length && !visited.contains(down) && down>=0){
-                    visited.add(down);
-                    q.offer(down);
-                }
-            }
-        }
-        return false;
+        boolean[] visited=new boolean[arr.length];
+        // visited[start]=true;
+        return canVisit(arr,start,visited);
+    }
+    private boolean canVisit(int[] arr,int startIndex,boolean[] visited){
+         if(startIndex>=arr.length || startIndex <0 || visited[startIndex]==true)
+            return false;
+         if(startIndex<arr.length && arr[startIndex]==0)
+            return true;
+        visited[startIndex]=true;
+        boolean left=canVisit(arr,startIndex+arr[startIndex],visited);
+        // visited[startIndex]=false;
+        boolean right=canVisit(arr,startIndex-arr[startIndex],visited);
+        return left || right;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//         Queue<Integer> q=new LinkedList<>();
+//         q.offer(start);
+//         Set<Integer> visited=new HashSet<>();
+//         visited.add(start);
+//         while(!q.isEmpty()){
+//             int size=q.size();
+//             for(int i=0;i<size;i++){
+//                 int k=q.poll();
+//                 if(arr[k]==0)
+//                     return true;
+//                 //check up directions is within boundry and not visited
+//                 int up=k+arr[k];
+//                 if(up<arr.length && !visited.contains(up) && up>=0){
+//                     visited.add(up);
+//                     q.offer(up);
+//                 }
+//                  //check down directions is within boundry and not visited
+//                 int down=k-arr[k];
+//                 if(down<arr.length && !visited.contains(down) && down>=0){
+//                     visited.add(down);
+//                     q.offer(down);
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// }

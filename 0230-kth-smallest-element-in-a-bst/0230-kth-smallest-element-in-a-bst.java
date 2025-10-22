c7b17e1f3c799e27610a7c29f448aa8d786be47c
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-10-21 17:19:13 -0700

    Time: 0 ms (100%), Space: 44.2 MB (94.76%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -15,22 +15,18 @@
  */
 class Solution {
     public int kthSmallest(TreeNode root, int k) {
-        //here as tree is BST ,we take advantage of the property that if it is travered in inorder,we get ascending order
-        //create an array of int in ascending order
-        //return kth elemnt in that array
-        List<Integer> res=new ArrayList<>();
-        inOrder(root,res);
-        // int[] ans=new int[res.size()];
-        // for(int i=0;i<res.size();i++){
-        //     ans[i]=res.get(i);
-        // }
-        return res.get(k-1);
+        List<TreeNode> sorted=new ArrayList<>();
+        //since this is a BST, we can do inorder so we will get sorted list,pick the ith value from it
+        bst(root,sorted);
+        return sorted.get(k-1).val;
+        
     }
-    void inOrder(TreeNode node,List<Integer> res){
-        if(node==null)
+    private void bst(TreeNode root,List<TreeNode> sorted){
+        if(root==null)
             return;
-        inOrder(node.left,res);
-        res.add(node.val);
-        inOrder(node.right,res);
+        bst(root.left,sorted);
+        sorted.add(root);
+        bst(root.right,sorted);
+        return;
     }
 }

b5ad7c2242dfa49991c38142a1662e61d1e7a135
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-10-22 16:11:24 -0700

    Time: 5 ms (45.27%), Space: 44.5 MB (79.83%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
@@ -1,35 +1,33 @@
 class Solution {
-
+    int count;
+    int[] parent;
     public int countComponents(int n, int[][] edges) {
-        //convert edges list to adj list 
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        int count = 0;
-        boolean[] visited = new boolean[n];
-        for (int i = 0; i < n; i++) {
-            graph.put(i, new ArrayList<>());
-        }
-        for (int[] edge : edges) {
-            graph.get(edge[0]).add(edge[1]);
-            graph.get(edge[1]).add(edge[0]);
-        }
-         //count=0,and visted[] boolean ,for each nide, i will check whether it is visted or not,if not visited ,new compnonent,increase count anll a method dfs
-        for(int i=0;i<n;i++){
-            if(!visited[i]){
-                count++;
-                dfs(i,graph,visited);
-            }
-
+        count=n;
+        //union find
+        parent=new int[n];
+        for(int i=0;i<n;i++)
+            parent[i]=i;
+        for(int[] edge:edges){
+            int x=edge[0];
+            int y=edge[1];
+            union(x,y);
         }
         return count;
-       
     }
-        //dfs methode check for all its neighbours and mark it. as visited
-      void dfs(int node,Map<Integer, List<Integer>> graph,boolean[] visited){
-        visited[node]=true;
-        for(int neighbour:graph.get(node)){
-            if(!visited[neighbour]){
-                dfs(neighbour,graph,visited);
+    private void union(int x,int y){
+            int px=find(x);
+            int py=find(y);
+            if(px==py)
+                return;
+            else{
+                parent[px]=find(py);
+                count--;
             }
-        }
-      }
+            return;
+    }
+    private int find(int x){
+        if(parent[x]==x)
+            return x;
+        return find(parent[x]);
+    }
 }

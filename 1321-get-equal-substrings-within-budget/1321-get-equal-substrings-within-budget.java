65b239f0c4bb616ea3f6a7e3c3ed68e91bf9a3a6
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-10-28 18:21:06 -0700

    Time: 5 ms (99.28%), Space: 43.8 MB (7.49%) - LeetHub

--- 1321-get-equal-substrings-within-budget/1321-get-equal-substrings-within-budget.java
+++ 1321-get-equal-substrings-within-budget/1321-get-equal-substrings-within-budget.java
@@ -1,91 +1,24 @@
 class Solution {
     public int equalSubstring(String s, String t, int maxCost) {
-        //two pointer 
-        int start=0;
-        int end=0;
-        int ans=Integer.MIN_VALUE;
-        int currCost=0;
-       while(end<t.length() && start<=end){
-         char c=t.charAt(end);//b
-         currCost+=Math.abs(c-s.charAt(end));
-        //   System.out.println(currCost);
-            if(currCost<=maxCost){
-                ans=Math.max(ans,end-start+1);
+        if (s.length() != (t.length()))
+            return 0;
+        int start = 0;
+        int end = 0;
+        int n = s.length();
+        int[] diff = new int[n];
+        int currCost = 0;
+        int maxLength = 0;
+        for (int i = 0; i < n; i++)
+            diff[i] = Math.abs(s.charAt(i) - t.charAt(i));
+        while (end < n) {
+            currCost += diff[end];
+            while (currCost > maxCost) {
+                currCost -= diff[start];
+                start++;
             }
-            else{
-                while(currCost>maxCost){
-                    // System.out.println(t.charAt(start));
-                    currCost-=Math.abs(t.charAt(start)-s.charAt(start));
-                    start++;
-                }
-            }  
-            end++;      
-       }
-       return ans;
+            maxLength = Math.max(maxLength, end - start + 1);
+            end++;
+        }
+        return maxLength;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//         int n = s.length();
-//         int l = 0, r = 0, ans = 0, curr = 0;
-//         int[] diff = new int[n];
-//         for (int i = 0; i < s.length(); i++) {
-
-//             diff[i] = Math.abs(s.charAt(i) - t.charAt(i));
-//            // System.out.println(diff[i]);
-//         }
-
-//         // when first cost is greater than maxcost
-//         while (r<n && diff[r] > maxCost) {
-//             l++;
-//             r++;
-//         }
-
-//         while (r < n) {
-//             curr += diff[r];
-            
-//             if (curr > maxCost) {
-//                 curr -= diff[l];
-//                 l++;
-//             }
-//             else {
-//                 ans = Math.max(ans, r - l + 1);
-//             }
-//             r++;
-//         }
-        
-        
-//         return ans;
-
-//     }
-
-// }

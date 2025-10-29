be159019abcf2ad76d5e4e32d5c14a01a1480f63
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-10-29 12:23:01 -0700

    Time: 0 ms (100%), Space: 42 MB (76.17%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -1,21 +1,18 @@
 class Solution {
     public int findMin(int[] nums) {
-        //just like finding pivot elemnts
-        int n=nums.length;
-        int left=0;
-        int right=n-1;
-        if(n==0)
-            return -1;
-        while(left<right){
-            int mid=left+(right-left)/2;
-            //left is sorted
-            if(nums[mid]>nums[right]){
-                left=mid+1;
+        //like finding pivot elemnt
+        //do a binary search
+        int start = 0;
+        int end = nums.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > nums[end]) {
+                //left is sorted check right
+                start = mid+1;
             }
             else
-                right=mid;
+                end=mid;
         }
-    // System.out.println("pivot ele is "+left);
-        return nums[left];
+        return nums[start];
     }
 }

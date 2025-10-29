76fd3448c3ec269d30766d2961fdcfcb41e2d0fa
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-10-28 19:41:14 -0700

    Time: 20 ms (11.13%), Space: 43.5 MB (69.83%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.java
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -14,66 +14,28 @@
  * }
  */
 class Solution {
-    
+    int target;
+    int sum;
+
     public int pathSum(TreeNode root, int targetSum) {
-        if(root==null)
+        target = targetSum;
+        if (root == null)
             return 0;
-        int ans=dfs(root,targetSum,0L);
-        int left=pathSum(root.left,targetSum);
-        int right=pathSum(root.right,targetSum);
-        return ans+left+right;
+      
+       return dfs(root, 0) + pathSum(root.left, targetSum) + pathSum(root.right, targetSum);
+
     }
-    public int dfs(TreeNode node,int targetSum,long currVal){
-        int ans=0;
-        if(node==null)
+
+    private int dfs(TreeNode node, long currSum) {
+        int count=0;
+        if (node==null)
             return 0;
-        currVal+=node.val;
-        if(currVal==targetSum)
-            ans++;
-        ans+=dfs(node.left,targetSum,currVal);
-        ans+=dfs(node.right,targetSum,currVal);
-        return ans;
+         currSum += node.val;
+         if (currSum == target)
+             count++;
+         count+=dfs(node.left, currSum);
+         count+=dfs(node.right, currSum);
+
+        return count;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // int ans;
-    // public int pathSum(TreeNode root, int targetSum) {
-    //      ans=0;
-    //      traverse(root,targetSum);
-    //      return ans;
-    // }
-    // private void traverse(TreeNode root, int targetSum){
-//          if(root==null)
-//             return;
-//         dfs(root,targetSum,0L);
-//         traverse(root.left,targetSum);
-//         traverse(root.right,targetSum);
-        
-//     }
-//     private void dfs(TreeNode root,int target,long curr){
-//         if(root==null)
-//             return;
-//         curr+=root.val;
-//         if(curr==target)
-//             ans++;
-//         dfs(root.left,target,curr);
-//         dfs(root.right,target,curr);
- 
-//     }
-// }

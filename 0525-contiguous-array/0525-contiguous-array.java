4fc980544611570e9ab817e35c842b5973331daa
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-10-28 17:04:08 -0700

    Time: 21 ms (86.86%), Space: 55.5 MB (33.12%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.java
+++ 0525-contiguous-array/0525-contiguous-array.java
@@ -1,23 +1,25 @@
 class Solution {
     public int findMaxLength(int[] nums) {
-        Map<Integer,Integer> prefixSum=new HashMap<>();
-        prefixSum.put(0,-1);
-        int sum=0;
-        int maxLength=0;
+        //replace 0 with -1
+        int max=0;
+        int[] prefix=new int[nums.length];
+        Map<Integer,Integer> dict=new HashMap<>();
         for(int i=0;i<nums.length;i++){
-            int n=nums[i];
-            if(n==0)
-                sum-=1;
-            else
-                sum+=1;
-            if(prefixSum.containsKey(sum)){
-                maxLength=Math.max(maxLength,i-prefixSum.get(sum));
-            }
-            else{
-                prefixSum.put(sum,i);
-            }
+            if(nums[i]==0)
+                nums[i]=-1;
+            
         }
-        return maxLength;
-
+       prefix[0]=nums[0];
+       for(int i=1;i<nums.length;i++)
+            prefix[i]=prefix[i-1]+nums[i];
+        dict.put(0,-1);
+        for(int i=0;i<nums.length;i++){
+            if(dict.containsKey(prefix[i]))
+                 max=Math.max(max,i-dict.get(prefix[i]));
+            else
+                dict.put(prefix[i],i);
+        }
+        
+         return max;
     }
 }

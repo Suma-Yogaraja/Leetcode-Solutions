eebc2a22c45c6d716612541eb06456d551a43fd9
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-10-31 12:17:06 -0700

    Time: 29 ms (77.73%), Space: 96 MB (6.3%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,25 +1,21 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        //first use set
         Set<Integer> seen=new HashSet<>();
-        for(int j:nums){
-            seen.add(j);
-        }
-        //int length=0;
+        int length=0;
         int maxLength=0;
+        for(int num:nums)
+            seen.add(num);
         for(int num:seen){
-            int length=0;
-            //int inc=1;
-            int curr=num;
-            if(!seen.contains(curr-1)){
-                length++;
-                while(seen.contains(curr+1)){
+            if(!seen.contains(num-1)){
+                length=1;
+                int i=1;
+                while(seen.contains(num+i)){
                     length++;
-                    curr++;
+                    i++;
                 }
                 maxLength=Math.max(maxLength,length);
             }
         }
-    return maxLength;
+        return maxLength;
     }
 }

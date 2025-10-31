bbba67eecaffac0c329c769d7ee0ed09f55da7aa
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-10-31 12:45:06 -0700

    Time: 1 ms (58.35%), Space: 48.7 MB (5.48%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,26 +1,21 @@
 class Solution {
     public int trap(int[] height) {
-        //find left max and right max for each index 
-        if (height == null || height.length == 0) 
-            return 0;
-        int[] leftMax=new int[height.length];
-        int[] rightMax=new int[height.length];
-        leftMax[0]=height[0];
-        rightMax[height.length-1]=height[height.length-1];
-        //System.out.println(leftMax[0] + "  " + rightMax[height.length-1]);
-        for(int i=1;i<height.length;i++){
-            leftMax[i]=Math.max(leftMax[i-1],height[i]);
-            
-        }
-        for(int j=height.length-2;j>=0;j--){
-            rightMax[j]=Math.max(rightMax[j+1],height[j]);
-            //System.out.print(rightMax[j]);
-        }
+        int n=height.length;
         int totalWater=0;
-        for(int i=0;i<height.length;i++){
+        int[] leftMax=new int[n];
+        int[] rightMax=new int[n];
+        leftMax[0]=height[0];
+        rightMax[n-1]=height[n-1];
+        for(int i=1;i<n;i++){
+            leftMax[i]=Math.max(height[i],leftMax[i-1]);
+           
+        }
+        for(int j=n-2;j>=0;j--){
+            rightMax[j]=Math.max(height[j],rightMax[j+1]);
+        }
+        for(int i=0;i<n;i++){
             totalWater+=Math.max(0,(Math.min(leftMax[i],rightMax[i])-height[i]));
         }
         return totalWater;
-        
     }
 }

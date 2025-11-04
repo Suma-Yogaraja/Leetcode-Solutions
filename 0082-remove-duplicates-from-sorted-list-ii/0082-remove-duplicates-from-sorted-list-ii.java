c323423aa18d5a336a209510006da0937730a2ec
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-11-04 14:02:30 -0800

    Time: 0 ms (100%), Space: 44.9 MB (5%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
@@ -10,61 +10,28 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        Map<Integer,Integer> map=new HashMap<>();
-        ListNode temp=head;
-        while(temp!=null){
-            map.put(temp.val,map.getOrDefault(temp.val,0)+1);
-            temp=temp.next;
-        }  
         ListNode dummy=new ListNode(0);
-        ListNode prev=dummy;
         dummy.next=head;
+        ListNode prev=dummy;
         ListNode curr=head;
+        ListNode nextNode=curr;
+        boolean flag=false;
         while(curr!=null){
-            if(map.get(curr.val)>1){
-                prev.next=curr.next;
+            nextNode=curr.next;
+            while(nextNode!=null && curr.val==nextNode.val){
+                nextNode=nextNode.next;
+                flag=true;
+            }
+            if(flag){
+                prev.next=nextNode;
+                flag=false;
             }
             else{
-                prev=prev.next;
-            }   
-               curr=curr.next;         
+                prev.next=curr;
+                prev=curr;
+            }
+            curr=nextNode;
         }
-       return dummy.next;
+        return dummy.next;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-        // //first create dummy and 
-        // ListNode dummy=new ListNode(0);
-        // ListNode prev=dummy;
-        // ListNode curr=head;
-        // dummy.next=head;
-        // if(head==null || (head.val==head.next.val && head.next.next ==null) )
-        //     return null;
-        // if(head.next==null)
-        //     return head;
-        // while(curr!=null && curr.next!=null){
-        //     if(curr.val==curr.next.val){
-        //     while(curr.val ==curr.next.val && curr.next!=null){
-        //         curr=curr.next;
-        //     }
-        //     prev.next=curr.next;
-        //     }
-        //     else{
-        //         prev=prev.next;
-        //     }
-        //     curr=curr.next;
-        // }
-
-        // return dummy.next;     
-        
- 

fab2e529c9891463e27071c54774b416f2b57143
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-11-03 16:04:59 -0800

    Time: 9 ms (78.14%), Space: 46.2 MB (13.26%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -1,72 +1,49 @@
 class Solution {
-    public static class Pair {
-        int number;
-        int sign;
-
-        public Pair(int number, int sign) {
-            this.number = number;
-            this.sign = sign;
-        }
-    }
-
     public int calculate(String s) {
-        //sign,result,stack and num,operand
-        //num->start building number
-        //sign-set sign=this curr sign
-        //any operand(+ or -),result+=sign*num
-        //(-calculate the curr result ,push sign and result to stack
-        //)-caclulate curr onr and pop prev ,result=prev+num*sign
-        //" " ignore
-        Stack<Pair> stack = new Stack<>();//{result,sign}
-        int result = 0;
-        int sign = 1;
-        int num = 0;
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            switch (c) {
-                case '+':
-                    if (sb.length() > 0) {
-                        num = Integer.parseInt(sb.toString());
-                        result += num * sign;
-                        sb.setLength(0);
-                    }
-                    sign = 1;
-                    break;
-                case '-':
-                    if (sb.length() > 0) {
-                        num = Integer.parseInt(sb.toString());
-                        result += num * sign;
-                        sb.setLength(0);
-                    }
-                    sign = -1;
-                    break;
-                case '(':
-                    stack.push(new Pair(result, sign));
-                    result = 0;
-                    sign = 1;
-                    break;
-                case ')':
-                    if (sb.length() > 0) {
-                        result += sign * Integer.parseInt(sb.toString());
-                        sb.setLength(0);
-                    }
-                    Pair prev = stack.pop();
-                    int prevResult = prev.number;
-                    int prevSign = prev.sign;
-                    result = prevResult + prevSign * result;
-                    break;
-                case ' ':
-                    break;
-                default:
-                    sb.append(c);
+        Stack<int[]> stack=new Stack<>();
+        int sign=1;
+        int result=0;
+        int num=0;
+        char[] nums=s.toCharArray();
+        for(int i=0;i<nums.length;i++){
+            char n=nums[i];
+            if(Character.isDigit(n)){
+                //digit so start building number
+                num=num*10 +(int)(n - '0');;
+                // System.out.println(num);
+            }
+            else{
+                switch(n){
+                    case ' ':
+                            break;
+                    case '+':
+                        result+=sign*num;
+                        num=0;
+                        sign=1;
+                        break;
+                    case '-':
+                        result+=sign*num;
+                        num=0;sign=-1;
+                        break;
+                    case '(':
+                        stack.push(new int[]{result,sign});
+                        result=0;
+                        sign=1;
+                        break;
+                    case ')':
+                        result+=sign*num;
+                        num=0;
+                        int res[]=stack.pop();
+                        result=result*res[1];
+                        result+=res[0];
+                        break;
+                    default:
+                        
+                }
+                // prevOp=n;
             }
         }
-        //if there are any nums left we have to process
-        if (sb.length() > 0) {
-            num = Integer.parseInt(sb.toString());
-            result += num * sign;
-        }
+        result+=sign*num;
         return result;
     }
 }

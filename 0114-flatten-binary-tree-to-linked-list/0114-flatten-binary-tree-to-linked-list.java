a082e48cb216afa2081d9f0e17c3bd844ce575fb
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-11-05 12:25:37 -0800

    Time: 0 ms (100%), Space: 43.8 MB (5.31%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -18,19 +18,58 @@ class Solution {
     public void flatten(TreeNode root) {
         if(root==null)
             return;
-        arr=new ArrayList<>();
-        preOrder(root);
-        for(int i=1;i<arr.size();i++){
-            root.left=null;
-            root.right=arr.get(i);
-            root=root.right;
+        TreeNode curr=root;
+        while(curr!=null){
+            if(curr.left!=null){
+                TreeNode temp=curr.left;
+                while(temp.right!=null)
+                    temp=temp.right;
+                temp.right=curr.right;
+                curr.right=curr.left;
+                curr.left=null;
+
+            }
+            curr=curr.right;
         }
     }
-    private void preOrder(TreeNode node){
-        if(node==null)
-            return;
-        arr.add(node);
-        preOrder(node.left);
-        preOrder(node.right);
-    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//         if(root==null)
+//             return;
+//         arr=new ArrayList<>();
+//         preOrder(root);
+//         for(int i=1;i<arr.size();i++){
+//             root.left=null;
+//             root.right=arr.get(i);
+//             root=root.right;
+//         }
+//     }
+//     private void preOrder(TreeNode node){
+//         if(node==null)
+//             return;
+//         arr.add(node);
+//         preOrder(node.left);
+//         preOrder(node.right);
+//     }
+// }

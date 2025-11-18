b5db642cfa6e690e87395557fe3bf9d791fa1f3c
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-11-18 10:56:37 -0800

    Time: 4 ms (97.06%), Space: 57.6 MB (20.73%) - LeetHub

--- 1474-longest-zigzag-path-in-a-binary-tree/1474-longest-zigzag-path-in-a-binary-tree.java
+++ 1474-longest-zigzag-path-in-a-binary-tree/1474-longest-zigzag-path-in-a-binary-tree.java
@@ -16,30 +16,27 @@
 class Solution {
     int maxPath;
     public int longestZigZag(TreeNode root) {
-        maxPath=Integer.MIN_VALUE;
         if(root==null)
             return 0;
-        int lc=0;
-        int rc=0;
-        //left =true
-        dfs(root.left,1,rc,true);
-        dfs(root.right,lc,1,false);
+         maxPath=Integer.MIN_VALUE;
+        int lCount=0;
+        int rCount=0;
+        dfs(root.left,1,rCount,true);
+        dfs(root.right,lCount,1,false);
         return maxPath==Integer.MIN_VALUE?0:maxPath;
-
     }
     private void dfs(TreeNode node,int lc,int rc,boolean direction){
         if(node==null)
             return;
         maxPath=Math.max(maxPath,lc+rc);
         if(direction){
-            //left is true right is false,now go right
             dfs(node.right,lc,rc+1,false);
             dfs(node.left,1,0,true);
         }
         else{
-             dfs(node.left,lc+1,rc,true);
+            dfs(node.left,lc+1,rc,true);
             dfs(node.right,0,1,false);
         }
+        return;
     }
-
 }

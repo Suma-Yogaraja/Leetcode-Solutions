399548a35c1c818be594b40dcf711db73e93b1e8
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-11-20 11:25:12 -0800

    Time: 7 ms (30.87%), Space: 47.5 MB (5.69%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
@@ -1,33 +1,31 @@
 class Solution {
-    int count;
-    int[] parent;
     public int countComponents(int n, int[][] edges) {
-        count=n;
-        //union find
-        parent=new int[n];
+        int count=0;
+        //since this is edge we might need map
+        Map<Integer,List<Integer>> dict=new HashMap<>();
         for(int i=0;i<n;i++)
-            parent[i]=i;
+             dict.put(i,new ArrayList<>());
         for(int[] edge:edges){
             int x=edge[0];
             int y=edge[1];
-            union(x,y);
+            dict.get(x).add(y);
+            dict.get(y).add(x);
+        }
+        boolean[] visited=new boolean[n];
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                count++;
+                dfs(i,dict,visited);
+            }
         }
         return count;
     }
-    private void union(int x,int y){
-            int px=find(x);
-            int py=find(y);
-            if(px==py)
-                return;
-            else{
-                parent[px]=find(py);
-                count--;
+    private void dfs(int node,Map<Integer,List<Integer>> dict,boolean[] visited){
+        visited[node]=true;
+        for(int neighbour:dict.get(node)){
+            if(!visited[neighbour]){
+                dfs(neighbour,dict,visited);
             }
-            return;
-    }
-    private int find(int x){
-        if(parent[x]==x)
-            return x;
-        return find(parent[x]);
+        }
     }
 }

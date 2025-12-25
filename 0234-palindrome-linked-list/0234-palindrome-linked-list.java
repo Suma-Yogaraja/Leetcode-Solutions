ada0bff774c8f5566e206a3072d63969ab14b7e3
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-12-24 21:47:49 -0800

    Time: 5 ms (54.86%), Space: 93.6 MB (87.13%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,38 +10,34 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        //get middle of element usinf fast ans slow pointer
-        ListNode dummy=head; 
+        //thinking of finding mid and then reverse second part and check
         ListNode slow=head;
         ListNode fast=head;
-        int n=1;
+        ListNode prev=slow;
         while(fast!=null && fast.next!=null){
+            prev=slow;
             fast=fast.next.next;
             slow=slow.next;
         }
-       // System.out.println(slow.val);
-        dummy=slow;
-        ListNode curr=dummy;
-        ListNode prev=null;
-
-        //reverse either first or second,here im reversing second half
+        System.out.println(prev.val);
+        prev.next=null;
+        ListNode dummy=new ListNode();
+        ListNode curr=slow;
+        dummy.next=curr;
+        prev=null;
         while(curr!=null){
             ListNode nextNode=curr.next;
             curr.next=prev;
             prev=curr;
             curr=nextNode;
+            
         }
-       // System.out.println(prev.val);
-    
-        //now check head with second half(prev is head of second half)
-        while(prev!=null){
-            if(head.val!=prev.val){
+        while(head!=null){
+            if(head.val!=prev.val)
                 return false;
-            }
             head=head.next;
             prev=prev.next;
         }
-
         return true;
     }
 }

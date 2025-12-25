881fbd555505222ba0e42eb03d9a951e2f70c438
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-12-24 17:27:41 -0800

    Time: 0 ms (100%), Space: 43.4 MB (50.78%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,27 +1,16 @@
 class Solution {
     public void sortColors(int[] nums) {
-        //we have n value as o 1 0r 2
-        int[] count=new int[3];//since number can be either 0 , 1 or 2
-        if(nums.length<=1)
-            return ;
-        for(int num:nums){
-            count[num]++;
+        //we can use hashmap/array to check the number of zeros ones and tows 
+        int[] freq=new int[3];//sice we have only 0,1 or 2
+        for(int i=0;i<nums.length;i++){
+            freq[nums[i]]++;
         }
-        // for(int i=0;i<3;i++){
-        //     System.out.println(count[i]);
-        // }
         int j=0;
-        int i=0;
-        while(i<nums.length){
-            // System.out.println(count[j]);
-            if(count[j]==0){
-                j++;
-                continue;
+        for(int i=0;i<freq.length;i++){
+            while(freq[i]>0){
+                nums[j++]=i;
+                freq[i]--;
             }
-            int numberCount=j;
-            nums[i]=numberCount;
-            count[j]--;
-            i++;
         }
     }
 }

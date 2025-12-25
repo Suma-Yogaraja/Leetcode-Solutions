8c03ee59f4830c980b3579aead6a8941dff00b7f
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-12-24 19:45:18 -0800

    Time: 0 ms (100%), Space: 45.4 MB (14.29%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
@@ -8,17 +8,24 @@
  */
 class Solution {
     public void deleteNode(ListNode node) {
-        //delete doesnt mean to literally remove it
+        // //time complexity=O(n) n=lengthof linkedlist-length of node
+        // //space complexity=O(1)
+        // //delete doesnt mean to literally remove it
         // ListNode duplicate=node;
-        ListNode nextNode=node.next;
-        ListNode prev=node;
-        while(nextNode!=null){
-            node.val=nextNode.val;
-            prev=node;
-            node=nextNode;
-            nextNode=nextNode.next;
-        }
-        prev.next=null;
+
+        // ListNode nextNode=node.next;
+        // ListNode prev=node;
+        // while(nextNode!=null){
+        //     node.val=nextNode.val;
+        //     prev=node;
+        //     node=nextNode;
+        //     nextNode=nextNode.next;
+        // }
+        // prev.next=null;
+
+        //2n way
+        node.val=node.next.val;
+        node.next=node.next.next;
 
     }
 }

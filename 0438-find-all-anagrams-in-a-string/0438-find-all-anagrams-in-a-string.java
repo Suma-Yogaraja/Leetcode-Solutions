2dc46536c6122f4eba280c518384462a17c2eb6f
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-12-26 19:29:49 -0800

    Time: 311 ms (12.68%), Space: 49.3 MB (5.62%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,98 +1,37 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        //edge case slength less than p
-        if(s.length()<p.length())
-            return new ArrayList<>();
-        //now take two hash map for s and p string
-        Map<Character,Integer> sFre=new HashMap<>();
-        Map<Character,Integer> pFre=new HashMap<>();
+        //fixed sliding window
+        //space complexity=usinh hashmap so O(n) n->p's length
+        //time complexity=O(n+m)->s and p's length
         List<Integer> ans=new ArrayList<>();
-        //creating a map for p string
-        for(char c: p.toCharArray()){
-            pFre.put(c,pFre.getOrDefault(c,0)+1);
-        }
-        int l=p.length();
-        //initilase a window of size p and add elements to s
-        for(int i=0;i<l;i++){
-            sFre.put(s.charAt(i),sFre.getOrDefault(s.charAt(i),0)+1);
-        }
-        if(sFre.equals(pFre))//if first window is equal then we got an anagram with index zero
+        int start=0;
+        if(p.length()>s.length())
+            return ans;
+        Map<Character,Integer> dict=new HashMap<>();
+        for(char c:p.toCharArray())
+            dict.put(c,dict.getOrDefault(c,0)+1);
+        int end=p.length();
+        Map<Character,Integer> temp=new HashMap<>();
+        //create a fixed window of lengthp.length()
+        for(int i=0;i<p.length();i++)
+            temp.put(s.charAt(i),temp.getOrDefault(s.charAt(i),0)+1);
+        if(dict.equals(temp))
             ans.add(0);
-        for(int j=l;j<s.length();j++){
-            char last=s.charAt(j);
-            char first=s.charAt(j-l);
-            //add last character,check for freq of last character to be added and remove accordingly
-            sFre.put(last,sFre.getOrDefault(last,0)+1);
-            int firstCharCount=sFre.get(first);
-            if(firstCharCount==1)
-                sFre.remove(first);
-            else{
-                sFre.put(first,sFre.getOrDefault(first,0)-1);
+        while(end<s.length()){
+            System.out.println("inside while loop :" + s.charAt(end));
+            temp.put(s.charAt(end),temp.getOrDefault(s.charAt(end),0)+1);
+            int value=temp.get(s.charAt(start))-1;
+            if(value==0)
+                temp.remove(s.charAt(start));
+            else
+                temp.put(s.charAt(start),temp.getOrDefault(s.charAt(start),0)-1);
+            start++;
+            if(dict.equals(temp)){
+                // System.out.println("start character is " + s.charAt(start));
+                ans.add(start);
             }
-            if(sFre.equals(pFre))
-                ans.add(j-l+1);
+            end++;
         }
         return ans;
-    }   
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//             //also create a hash map for p
-//             if(s.length()<p.length())
-//                 return new ArrayList<Integer>();
-            
-//             Map<Character,Integer> pFreq=new HashMap<>();
-//             Map<Character,Integer> sFreq=new HashMap<>();
-//             List<Integer> res=new ArrayList<>();
-//             int len=p.length();
-//             for(char c:p.toCharArray()){
-//                 pFreq.put(c,pFreq.getOrDefault(c,0)+1);
-//             }
-//             //first create a window of size p,
-//             for(int i=0;i<len;i++){
-//                 sFreq.put(s.charAt(i),sFreq.getOrDefault(s.charAt(i),0)+1);
-//             }
-//            if(sFreq.equals(pFreq))
-//              res.add(0);
-            
-//         //then do sliding window of fixed size
-//         for(int j=len;j<s.length();j++){
-//             char last=s.charAt(j);
-//             char first=s.charAt(j-len);
-//             sFreq.put(last,sFreq.getOrDefault(last,0)+1);
-//             int firstCount=sFreq.get(first);
-//             if(firstCount==1){
-//                 sFreq.remove(first);
-//             }
-//             else
-//             {
-//             sFreq.put(first,sFreq.getOrDefault(first,0)-1);
-//             }
-//             //System.out.println(sFreq + ":" + pFreq);
-//              if(sFreq.equals(pFreq)){
-//                     res.add(j-len+1);
-//                     //System.out.println("adding " + j);
-//              }
-                
-
-//         }
-//         return res;
-//     }
-// }

97ee156ddb7d6e5837d268d7b252a85b5e75e4ef
Suma-Yogaraja <suma.yogaraja@gmail.com>
2025-12-29 19:13:09 -0800

    Time: 3 ms (55.51%), Space: 44.7 MB (77.46%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,47 +1,17 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        //keep both array in 2 sets and check for intersection elemnts
-        //this way we can remove duplicates
-        //sort both the array and take tow pointers,check if elemnt at i=i-1,if so duplicate is present
-        //soincrement the pointer ,same holds for other array
-        Set<Integer> num1=new HashSet<>();
-        Set<Integer> result=new HashSet<>();
-        for(int i:nums1){
-            num1.add(i);
+        List<Integer> ans=new ArrayList<>();
+        Set<Integer> set=new HashSet<>();
+        for(int num:nums1)
+            set.add(num);
+        for(int num:nums2){
+            if(set.contains(num)&& !ans.contains(num))
+                ans.add(num);
         }
-        for(int i:nums2){
-            if(num1.contains(i)){
-                result.add(i);
-            }
-        }
-        int[] res=new int[result.size()];
-        int k=0;
-        for(int j:result){
-            res[k]=j;
-            k++;
+        int[] res=new int[ans.size()];
+        for(int i=0;i<ans.size();i++){
+            res[i]=ans.get(i);
         }
         return res;
     }
 }
-
-
-
-
-
-
-
-
-
-//         for(int i:nums2){
-//             num2.add(i);
-//         }
-//         for(int j:num1){
-//             if(num2.contains(j)){
-//                 result.add(j);
-//             }
-//         }
-//         int[] res=result.stream().mapToInt(Integer::intValue).toArray();
-
-//         return res;
-//     }
-// }

245d40666f0c7ee44acf7c21ad929846ff593b6e
Suma-Yogaraja <suma.yogaraja@gmail.com>
2026-01-07 13:30:51 -0800

    Time: 1 ms (99.71%), Space: 47.5 MB (69.49%) - LeetHub

---
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String firstPalindrome(String[] words) {
+        if(words.length<1)
+            return null;
+        for(int i=0;i<words.length;i++){
+            if(isPalindrome(words[i]))
+                return words[i];
+        }
+        return new String();
+    }
+    private boolean isPalindrome(String s){
+        int r=s.length()-1;
+        int l=0;
+        while(l<=r){
+            if(s.charAt(l)!=s.charAt(r))
+                return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+}

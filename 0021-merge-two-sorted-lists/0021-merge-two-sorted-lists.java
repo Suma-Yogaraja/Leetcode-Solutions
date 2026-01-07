d1dae389b428aee38643dda6eb08b5d398f3272b
Suma-Yogaraja <suma.yogaraja@gmail.com>
2026-01-07 14:50:40 -0800

    Time: 0 ms (100%), Space: 44.2 MB (58.27%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -39,15 +39,11 @@ class Solution {
             }
             head=head.next;
         }
-        while(list1!=null){
+        if(list1!=null){
             head.next=list1;
-             list1=list1.next;
-             head=head.next;
         }
-        while(list2!=null){
+        if(list2!=null){
             head.next=list2;
-             list2=list2.next;
-             head=head.next;
         }
         return dummy;
     }

377c78bfb7ce7230eabe545305ad7a94e658fb9c
Suma-Yogaraja <suma.yogaraja@gmail.com>
2026-01-07 16:22:04 -0800

    Time: 0 ms (100%), Space: 43 MB (19.31%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -11,11 +11,11 @@ class Solution {
         else{
             maxSoFar[0]=nums[0];
             maxSoFar[1]=Math.max(nums[0],nums[1]);
-             System.out.println(maxSoFar[0]);
+            //  System.out.println(maxSoFar[0]);
         }
         for(int i=2;i<nums.length;i++){
                 maxSoFar[i]=Math.max(nums[i]+maxSoFar[i-2],maxSoFar[i-1]);
-                System.out.println(maxSoFar[i]);
+                // System.out.println(maxSoFar[i]);
         }
         return Math.max(maxSoFar[n-1],maxSoFar[n-2]);
     }

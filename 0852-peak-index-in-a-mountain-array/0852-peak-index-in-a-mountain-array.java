ae72fd354ece2f2dd8769eb8e65af73c3906cbd8
Suma-Yogaraja <suma.yogaraja@gmail.com>
2026-01-09 15:57:09 -0800

    Time: 3 ms (2.73%), Space: 80.7 MB (10%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int low=0;
+        int high=arr.length-1;
+        while(low<high){
+            int mid=low+(high-low)/2;
+             System.out.println(mid);
+            if(arr[mid]<arr[mid+1]) //left is sorted
+                low=mid+1;
+            else
+                high=mid;
+              
+        }
+           
+        return low;
+    }
+}

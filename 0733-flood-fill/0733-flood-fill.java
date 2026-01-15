ec695595786da665ca0c92da693a75db4f22fc12
Suma-Yogaraja <suma.yogaraja@gmail.com>
2026-01-14 19:40:34 -0800

    Time: 0 ms (100%), Space: 46.6 MB (91.17%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,42 +1,16 @@
 class Solution {
-    int[][] grid;
-    int[][] dir;
-    int m,n;
-    boolean[][] seen;
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        //dfs
-        if(image[sr][sc]==color)
-            return image;
-             m=image.length;
-         n=image[0].length;
-       seen=new boolean[m][n];
-        
-        Queue<int[]> q=new LinkedList<>();
-        dir=new int[][]{{-1,0},{1,0},{0,-1},{0,1}};
-        q.add(new int[]{sr,sc});
-        int prevColor=image[sr][sc];
-        image[sr][sc] = color;
-        while(!q.isEmpty()){
-            int size=q.size();
-            for(int i=0;i<size;i++){
-                int[] cell=q.poll();
-                int row=cell[0];
-                int col=cell[1];
-                for(int[] direct:dir){
-                    int nextR= row +direct[0];
-                    int nextC=col+direct[1];
-                    if(isValid(nextR,nextC) && !seen[nextR][nextC] && image[nextR][nextC]==prevColor){
-                                seen[nextR][nextC]=true;
-                                image[nextR][nextC] = color;
-                        q.offer(new int[] { nextR, nextC });
-                        }
-                }
-            }
-        }
+        paint(image, image[sr][sc], sr,sc,color);
         return image;
     }
-    private boolean isValid(int nextR,int nextC){
-        return nextR >= 0 && nextC >= 0 && nextR < m && nextC < n;
-    }
+    private void paint(int[][]image, int originColor, int sr, int sc, int color){
+        if(sr<0 || sc<0 || sr>=image.length || sc>=image[0].length || image[sr][sc] != originColor || image[sr][sc] == color) return;
+        image[sr][sc] = color;
 
+        paint(image, originColor, sr-1, sc, color);
+        paint(image, originColor, sr+1, sc, color);
+        paint(image, originColor, sr, sc-1, color);
+        paint(image, originColor, sr, sc+1, color);
+        return;
+    }
 }

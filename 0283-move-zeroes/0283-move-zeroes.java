7834403504e9ef11e422790b64c4062b55e8b565
Suma-Yogaraja <suma.yogaraja@gmail.com>
2026-02-10 11:50:12 -0800

    Time: 2 ms (90.03%), Space: 47.8 MB (54.59%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,51 +1,27 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        if(nums.length<=1)
-         return;
-         int j=0;
-        for(int i=0;i<nums.length;i++){
+
+        //use two pointer
+        int j = -1;
+        //find first zero
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                j = i;
+                break;
+
+            }
+        }
+        if(j==-1)//no zeros in the array
+            return;
+        for(int i=j+1;i<nums.length;i++){
             if(nums[i]!=0){
-                nums[j]=nums[i];
+                //swap
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
                 j++;
             }
         }
-       
-        while(j<nums.length){
-            nums[j++]=0;
-        }
+
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-//             if(nums.length==1)
-//             return;
-
-//             int left=0,right=0,n=nums.length;
-
-//             while(right<n){
-//                 if(nums[right]==0){
-//                     right++;
-//                 }
-//                 else {
-//                     if(nums[right]!=0 && nums[left]==0){
-//                         int temp=nums[left];
-//                         nums[left]=nums[right];
-//                         nums[right]=temp;
-//                     }
-//                     left++;
-//                     right++;
-//                 }
-//             }
-//     }
-// }

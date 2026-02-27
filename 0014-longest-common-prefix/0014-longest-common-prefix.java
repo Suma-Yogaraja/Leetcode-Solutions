f660431dedd05e5dac3223574a33e88a607818b3
Suma-Yogaraja <suma.yogaraja@gmail.com>
2026-02-27 12:11:59 -0800

    Time: 1 ms (65.29%), Space: 43 MB (92.92%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,41 +1,21 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if (strs.length == 0 || strs == null)
-            return "";
-        if (strs.length == 1)
+        //sort and comoare first and last string
+        if(strs.length<=0)
+            return null;
+        if(strs.length==1)
             return strs[0];
-        for (int i = 0; i < strs[0].length(); i++) {
-            char c = strs[0].charAt(i);
-            // System.out.println(c);
-            for(int j=1;j<strs.length;j++){
-                if(i>=strs[j].length() || c!=strs[j].charAt(i))
-                    return strs[0].substring(0,i);
-            }
+        Arrays.sort(strs);
+        String end=strs[strs.length-1];
+        StringBuilder ans=new StringBuilder();
+        for(int i=0;i<Math.min(strs[0].length(),end.length());i++){
+            if(strs[0].charAt(i)!=end.charAt(i))
+                break;
+            else
+                ans.append(strs[0].charAt(i));
         }
-        return strs[0];
+        if(ans.length()==0)
+            return "";
+        return ans.toString();
     }
 }
-
-
-
-
-
-
-
-
-//         if(strs==null || strs.length==0)
-//             return "";
-//             if(strs.length==1)
-//                 return strs[0];
-//         String prefix=strs[0];
-//         for(int i=0;i<prefix.length();i++){
-//             char compare=prefix.charAt(i);
-//             for(int j=1;j<strs.length;j++){
-//                 if(i>=strs[j].length()||  compare!=strs[j].charAt(i))
-//                     return prefix.substring(0,i);
-//             }
-//         }
-//        return prefix;
-
-//     }
-// }

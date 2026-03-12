371e92d70a1fd1020bb6e8582d9dcffa5f797503
Suma-Yogaraja <suma.yogaraja@gmail.com>
2026-03-12 11:47:10 -0700

    Time: 14 ms (69.25%), Space: 47.7 MB (46.48%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,20 +1,24 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        int[] ans ;
-        Map<Integer, Integer> freq = new HashMap<>();
-        for (int num : nums) {
-            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        //first thing first ,create freq dict to get freq
+        Map<Integer,Integer> dict=new HashMap<>();
+        for(int num:nums){
+            dict.put(num,dict.getOrDefault(num,0)+1);
         }
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->freq.get(a)-freq.get(b));
-        for(int key:freq.keySet()){
-            pq.add(key);
+        //min heap to store(elemnt,freq)
+        PriorityQueue<int[]> pq=new PriorityQueue<>(
+            (a,b)-> (a[1]-b[1])
+        );
+        int[] ans=new int[k];
+        for(int key:dict.keySet()){
+            pq.add(new int[]{key,dict.get(key)});
             if(pq.size()>k){
-                pq.remove();
+                pq.poll();
             }
         }
-        ans=new int[pq.size()];
-        for(int i=0;i<ans.length;i++){
-            ans[i]=pq.remove();
+        int i=0;
+        while(!pq.isEmpty()){
+            ans[i++]=pq.poll()[0];
         }
         return ans;
     }

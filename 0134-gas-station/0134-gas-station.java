caa39f4d5d84874b48fba6dba5757b980032aaa5
Suma-Yogaraja <suma.yogaraja@gmail.com>
2026-04-07 12:28:36 -0700

    Time: 1804 ms (5.04%), Space: 116.9 MB (99.16%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,46 +1,34 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
+        int totalCost = 0;
+        int totalGas = 0;
+        int tank = 0;
         if(gas.length==1 && gas[0]==cost[0])
             return 0;
-        int[] pathValue = new int[gas.length];
-        for (int i = 0; i < gas.length; i++) {
-            pathValue[i] = gas[i] - cost[i];
-            //System.out.println(pathValue[i]);
+        long[] pathValue = new long[gas.length];
+        for (int i = 0; i < pathValue.length; i++) {
+            pathValue[i] = (gas[i] - cost[i]);
         }
-        // List<Integer> possibleAnswer = new ArrayList<>();
-        // int max = 0;
-        // for (int i = 0; i < pathValue.length; i++) {
-        //     if (pathValue[max] < pathValue[i])
-        //         max = i;
-        // }
-        // possibleAnswer.add(max);
-
-        // for (int i = 0; i < pathValue.length; i++) {
-        //     if (pathValue[max] == pathValue[i] && i != max) {
-        //         possibleAnswer.add(i);
-        //         //System.out.println(i);
-        //     }
-        // }
-        for (int index=0;index<pathValue.length;index++) {
-            if(pathValue[index]>0){
-            if (isFeasible(index, pathValue))
-                return index;
+        for (int i = 0; i < pathValue.length; i++) {
+            if (pathValue[i] > 0) {
+                if (isFeasible(i, pathValue))
+                    return i;
             }
         }
         return -1;
     }
 
-    private boolean isFeasible(int startIndex, int[] pathValue) {
-        int count = 0;
-        int cost = 0;
-        while (count < pathValue.length) {
+    public boolean isFeasible(int startIndex,long[] pathValue){
+        long cost=0;
+        long count=0;
+        while(count<pathValue.length){
             count++;
-            if (cost + pathValue[startIndex] >= 0) {
-                cost += pathValue[startIndex];
-                startIndex = (startIndex + 1) % pathValue.length;
-            } else {
-                return false;
-            }
+                if(cost+pathValue[startIndex]>=0){
+                    cost+=pathValue[startIndex];
+                    startIndex=(startIndex+1)%pathValue.length;
+                }
+                else
+                    return false;   
         }
         return true;
     }

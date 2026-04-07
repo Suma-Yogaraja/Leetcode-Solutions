60c0a280e8e27b01dba405a6e8656c9585805936
Suma-Yogaraja <suma.yogaraja@gmail.com>
2026-04-07 11:47:03 -0700

    Time: 2 ms (95.03%), Space: 65.1 MB (72.96%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,26 +1,20 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        //prefix sum,suffix sum
-        //suffix produc[i]=nums[i-1]*nums[i-2]*.....*nums[0]
-        //prefix[i]=nums[i+1]*nums[i+2]*nus[i+3]*...nums[nums.length-1]
-        int[] res=new int[nums.length];
-        res[0]=1;
-        //first find prefix product
-        for(int i=1;i<nums.length;i++){
-            res[i]=res[i-1]*nums[i-1];
-        }
-        int[] suffixProduct=new int[nums.length];
-        suffixProduct[nums.length-1]=1;
-        for(int j=nums.length-2;j>=0;j--){
-            
-             suffixProduct[j]=suffixProduct[j+1]*nums[j+1]; 
-             res[j]=res[j]*suffixProduct[j];
-            // System.out.println(suffixProduct[j]);
+        //create suffix product and prefix product
+       
+        int[] prefixProd=new int[nums.length];
+        int[] suffixProd=new int[nums.length];
+        prefixProd[0]=1;
+        suffixProd[nums.length-1]=1;
+        for(int i=1;i<nums.length;i++)
+            prefixProd[i]=prefixProd[i-1]*nums[i-1];
+        for(int i=nums.length-2;i>=0;i--)
+            suffixProd[i]=suffixProd[i+1]*nums[i+1];
 
+        int[] ans=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            ans[i]=prefixProd[i]*suffixProd[i];
         }
-        // for(int i=0;i<nums.length;i++){
-        //     res[i]=res[i]*suffixProduct[i];
-        // }
-        return res;
+        return ans;
     }
 }

23da56ac0941b00243dea533b72d0dd6d7d177a9
Suma-Yogaraja <suma.yogaraja@gmail.com>
2026-04-07 12:42:31 -0700

    Time: 3 ms (86.88%), Space: 47.5 MB (76.58%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,27 +1,21 @@
 class Solution {
     public int candy(int[] ratings) {
-        //greedy
         int[] candy=new int[ratings.length];
-        Arrays.fill(candy,1);
-        for(int i=1;i<ratings.length;i++){
-            //look at left
-            if(ratings[i]>ratings[i-1])
+        Arrays.fill(candy,1);//each child must be atrleast 1 candy
+        
+        for(int i=1;i<candy.length;i++){
+            if(ratings[i]>ratings[i-1]){
                 candy[i]=candy[i-1]+1;
-               //System.out.println(candy[i]);
+            }
         }
         for(int i=candy.length-2;i>=0;i--){
-            //System.out.println(ratings[i]+ "  < " + ratings[i-1]);
-           // System.out.println(candy[i]+ "  > " + candy[i-1]);
-            if(ratings[i]>ratings[i+1]&& candy[i]<=candy[i+1]){
-                candy[i]=Math.max(candy[i],candy[i+1]+1);
-                
+            if(ratings[i]>ratings[i+1] && candy[i]<=candy[i+1]){
+                candy[i]=Math.max(candy[i+1]+1,candy[i]);
             }
-           // System.out.println(i  + " : " + candy[i]);
         }
-        int sum=0;
-        for(int c:candy){
+          int sum=0;
+          for(int c:candy)
             sum+=c;
-        }
         return sum;
     }
 }

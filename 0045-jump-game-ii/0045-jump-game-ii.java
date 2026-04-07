c55512692c6e37d376b5673eada32f9cb8d0d611
Suma-Yogaraja <suma.yogaraja@gmail.com>
2026-04-06 22:02:49 -0700

    Time: 1 ms (99.63%), Space: 47.6 MB (12.46%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,14 +1,13 @@
 class Solution {
     public int jump(int[] nums) {
+        int end=0;//end of the jump
         int jump=0;
-        int far=0;
-        int end=0;
+        int maxIndex=0;
         for(int i=0;i<nums.length-1;i++){
-            
-            far=Math.max(far,i+nums[i]);
-            if(i==end){
+            maxIndex=Math.max(maxIndex,i+nums[i]);
+            if(i==end){//current index is how far we reach 
                 jump++;
-                end=far;
+                end=maxIndex;
             }
         }
         return jump;

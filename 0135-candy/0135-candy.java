14c8dd1d9e4619778ec121bd5c1a93d5bf10313f
Suma-Yogaraja <suma.yogaraja@gmail.com>
2026-04-07 12:43:12 -0700

    Time: 3 ms (86.88%), Space: 47.4 MB (88.49%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -10,7 +10,8 @@ class Solution {
         }
         for(int i=candy.length-2;i>=0;i--){
             if(ratings[i]>ratings[i+1] && candy[i]<=candy[i+1]){
-                candy[i]=Math.max(candy[i+1]+1,candy[i]);
+                // candy[i]=Math.max(candy[i+1]+1,candy[i]);
+                candy[i]=candy[i+1]+1;
             }
         }
           int sum=0;

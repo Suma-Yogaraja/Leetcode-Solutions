59dc4deab6f3bad29fd9451f0dfd85f365e62bc0
Suma-Yogaraja <suma.yogaraja@gmail.com>
2026-04-07 18:50:25 -0700

    Time: 1 ms (99.93%), Space: 62 MB (77.89%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,63 +1,25 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        //two pointer
-        int start=0;
-        int end=0;
-        int sum=0;
+        int totalSum=0;
+        for(int num:nums)
+            totalSum+=num;
+        
+        if(totalSum<target)
+            return 0;
         int minLength=Integer.MAX_VALUE;
-        for(end=0;end<nums.length;end++){
-            sum+=nums[end];
+        int start=0;
+        int sum=0;
+        for(int end=0;end<nums.length;end++){
+             sum+=nums[end];
+            //  System.out.println("sum is " +sum);
             while(sum>=target){
-                minLength=Math.min(minLength,end-start+1);
+                // System.out.println("end is " +end);
+                minLength=Math.min(end-start+1,minLength);
                 sum-=nums[start];
                 start++;
             }
-           
+     
         }
-        return minLength==Integer.MAX_VALUE?0 :minLength;
+        return minLength==Integer.MAX_VALUE?0:minLength;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//         int left=0;
-//         int right=0;
-//         int sum=0;
-//         int res=Integer.MAX_VALUE;
-
-//         for(right=0;right<nums.length;right++)
-//         {   
-//              sum+=nums[right];
-//             while(sum>=target)
-//             {
-                
-
-//                 res=Math.min(res,right-left+1);
-//                 sum-=nums[left];
-//                 left++;
-//             }
-           
-//        // System.out.println(res);
-
-//         }
-    
-//         return res==Integer.MAX_VALUE ? 0 : res;
-
-//     }
-// }

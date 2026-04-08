bfbbeb6bcf3ca226c866d4a732291d1e38a673a5
Suma-Yogaraja <suma.yogaraja@gmail.com>
2026-04-08 15:45:40 -0700

    Time: 38 ms (5.37%), Space: 51.1 MB (6.75%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,55 +1,25 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        //first create frq[] array for each word and check for that key
-        Map<String,List<String>> map=new HashMap<>();
-        for(String s:strs){
-          char[] c=s.toCharArray();
-          Arrays.sort(c);
-          String key=new String(c);
-           // String key=sb.toString();
-            List<String> group=map.getOrDefault(key,new ArrayList<>());//if key not found
-            group.add(s);
-            map.put(key,group);
+        List<List<String>> ans=new ArrayList<>();
+        //map<word,index>
+        Map<String,List<Integer>> dict=new HashMap<>();
+       
+        for(int i=0;i<strs.length;i++){
+            String s=strs[i];
+            char[] c=s.toCharArray();
+            Arrays.sort(c);
+            String key=new String(c);
+            System.out.println(key);
+            dict.putIfAbsent(key, new ArrayList<>()); 
+            dict.get(key).add(i);
         }
-        return new ArrayList<>(map.values());
+        for(String key:dict.keySet()){
+            List<String> innerList=new ArrayList<>();
+            for(int value:dict.get(key)){
+                innerList.add(strs[value]);
+            }
+            ans.add(innerList);
+        }
+        return ans;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//         Map<String,List<String>> map=new HashMap<>();
-//         for(String s:strs){
-//             StringBuilder sb=new StringBuilder();
-//             int[] pattern=new int[26];
-//             for(char c: s.toCharArray()){
-//                 pattern[c-'a']++;
-//             }
-//             for(int i:pattern){
-//             sb.append(i);
-//             sb.append('#');
-//             }
-//             String key=sb.toString();
-//             List<String> group=map.getOrDefault(key,new ArrayList<>());
-//             group.add(s);
-//             map.put(key,group);
-           
-//         }
-    
-
-//     return new ArrayList<>(map.values());
-//     }
-// }

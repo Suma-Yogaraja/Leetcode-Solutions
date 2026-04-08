1418c0b97b43fa33cc4c3759033fd90b163548cb
Suma-Yogaraja <suma.yogaraja@gmail.com>
2026-04-08 16:13:57 -0700

    Time: 5 ms (57.16%), Space: 44.9 MB (58.44%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,34 +1,30 @@
 class Solution {
     public String simplifyPath(String path) {
-        //im thinking of a stack here
-        String[] s = path.split("/");
-        
-        Stack<String> stack = new Stack<>();
-        for (int i = 0; i < s.length; i++) {
-            String ch = s[i];
-            if (ch.isEmpty())
+       String[] paths=path.split("/");
+    //    for(String s:paths)
+    //     System.out.println(s);
+     Stack<String> stack=new Stack<>();
+        for(String s:paths){
+            if(s.equals(""))
                 continue;
-            switch (ch) {
-                case "."://do nothing
-                    break;
-                case "..":
-                    if (!stack.isEmpty()) {
-                        stack.pop();
-                    }
-                case " ":
-                    break;
-                default:
-                    stack.push(ch);
+            switch(s){  
+                case ".":
+                         break;
+                case ".." : if(!stack.isEmpty()){
+                                stack.pop();
+                            }
+                         break;
+                default: stack.push(s);
             }
-            //    System.out.println(ch);
         }
-        if(stack.isEmpty()) 
-            return "/";
         StringBuilder sb=new StringBuilder();
-        while (!stack.isEmpty()) {
-            sb.insert(0,stack.pop()).insert(0,"/");  
+        if(stack.isEmpty()){
+            return "/";
         }
-        // System.out.println(res[0]);
-        return sb.toString();
+        while(!stack.isEmpty()){
+            sb.insert(0,stack.pop());
+            sb.insert(0,"/");
+        }
+        return new String(sb);
     }
 }

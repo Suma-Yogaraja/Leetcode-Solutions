dabb13bf32ded66838ff778cfb12737560af0493
Suma-Yogaraja <suma.yogaraja@gmail.com>
2026-04-08 12:03:53 -0700

    Time: 6 ms (69.4%), Space: 46.5 MB (42.58%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,157 +1,18 @@
 class Solution {
-
     public int lengthOfLongestSubstring(String s) {
-        //use set along with sliding windo
-        Set<Character> unique=new HashSet<>();
-        int maxLength=0;
+        int maxLen=0;
         int start=0;
-        int end=0;
-        int n=s.length()-1;
-        while(end<=n){
-            char ch=s.charAt(end);
-            while(unique.contains(ch) && start<=end){//shrink window
-                    unique.remove(s.charAt(start));
+        Set<Character> set=new HashSet<>();
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            while(set.contains(c)){
+                //shrink
+                    set.remove(s.charAt(start));
                     start++;
-            }
-            unique.add(ch);
-            maxLength=Math.max(maxLength,unique.size());
-            end++;
+                } 
+                set.add(c);
+                maxLen=Math.max(maxLen,(i-start+1));
         }
-        return maxLength;
+        return maxLen;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//         //dynamic size sliding window using hashset
-//         Set<Character> set=new HashSet<>();//to add unique elemnts
-//         int maxCount=0;
-//         int j=0;
-//         for(int i=0;i<s.length();i++){
-//             char c=s.charAt(i);
-//             if(!set.contains(c)){
-//                 set.add(c);
-//                maxCount= Math.max(maxCount,set.size());
-//             }
-//             else{
-                
-//                 while(set.contains(c)){
-//                     set.remove(s.charAt(j));
-//                     j++;
-//                 }
-//                 set.add(c);
-                
-//             }
-//         }
-//         return maxCount;
-//     }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//         int first = 0;
-//         int last = 0;
-//         int maxLength=0;
-//         Set<Character> set = new HashSet<>();
-//         while (last < s.length()) {
-//             while (set.contains(s.charAt(last))) {
-//                 set.remove(s.charAt(first));
-//                 first++;
-//             }
-//                 set.add(s.charAt(last));
-//                 maxLength=Math.max(maxLength,set.size());
-//                // System.out.println(maxLength);
-//                 last++;
-//         }
-//         return maxLength;
-//     }
-// }
-
-//         Set<Character> seen = new HashSet<>();
-//         //use two pointer sliding window concept 
-//         int f = 0;
-//         int l = 0;
-//         int maxLen = 0;
-
-//         while (l < s.length()) {
-
-//             while (seen.contains(s.charAt(l))) {
-//                 //remove the left character and increment 
-//                 seen.remove(s.charAt(f));
-//                 f++;
-//             }
-//             seen.add(s.charAt(l));
-//             maxLen = Math.max(maxLen, seen.size());
-//             l++;
-//         }
-
-//         return maxLen;
-
-//     }
-// }
-
-//         Set<Character> seen=new HashSet<>();
-//         int l=0,r=0,maxlen=0;
-
-//             while(r<s.length()){
-
-//                 while(seen.contains(s.charAt(r))){
-
-//                     seen.remove(s.charAt(l));
-//                     l++;
-
-//                 }
-
-//                 seen.add(s.charAt(r));
-//                 maxlen=Math.max(maxlen,r-l+1);
-//                 r++;
-
-//             }
-
-//         return maxlen;
-
-//     }
-// }

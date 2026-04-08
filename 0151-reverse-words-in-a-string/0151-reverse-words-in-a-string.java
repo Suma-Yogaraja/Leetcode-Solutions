4ea51e89a7c091f3976a5b5aa646ffd3fbabeec5
Suma-Yogaraja <suma.yogaraja@gmail.com>
2026-04-07 17:18:45 -0700

    Time: 5 ms (86.51%), Space: 44.5 MB (53.94%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,31 +1,18 @@
 class Solution {
     public String reverseWords(String s) {
         s=s.trim();
-       // System.out.println(s);
-        String[] str=s.split(" ");
-        int start=0;
-        int end=str.length-1;
+        String[] split=s.split(" ");
         StringBuilder sb=new StringBuilder();
-        for(int i=end;i>=start;i--){
-            //str[i]=str[i].trim();
-            //System.out.println(" 1" + "" +str[i]);
-            if(str[i].equals(""))
-              continue;
-             sb.append(str[i]);
-             sb.append(" ");
+        for(int i=split.length-1;i>=0;i--){
+            if(split[i].equals(""))
+                continue;
+            sb.append(split[i]);
+            sb.append(" ");
         }
-       // return String.join(" ",res);
-       return sb.toString().trim();
+        
 
-        // StringBuilder sb=new StringBuilder();
-        // for(int i=end;i>=start;i--){
-        //     str[i].trim();
-        //     if(str[i]==" ")continue;
-        //     sb.append(str[i]);
-        //     if(i==start)
-        //         continue;
-        //     sb.append(" ");
-        // }
-        // return new String(sb);
+        return sb.toString().trim();
+
     }
+    
 }

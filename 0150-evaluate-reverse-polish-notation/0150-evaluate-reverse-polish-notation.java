51dbe05ca0da328d35746c4c2ff54412f0354624
Suma-Yogaraja <suma.yogaraja@gmail.com>
2026-04-08 16:36:15 -0700

    Time: 6 ms (91.27%), Space: 45.1 MB (82.4%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,42 +1,39 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<Integer> stack=new Stack<>();
         if(tokens.length<=0)
             return 0;
-        int sum=0;
-        for(int i=0;i<tokens.length;i++){
-            String n=tokens[i];
-            // System.out.println(n);
-            switch(n){
-                case "+":
-                            int num1=stack.pop();
-                            int num2=stack.pop();
-                            // System.out.println(num1+ " " + num2);    
-                            stack.push(num1+num2);
-                        break;
-                case "-":
-                        num1=stack.pop();
-                        num2=stack.pop();
-                        stack.push(num2-num1);
-                        break;
-                 case "/":
-                       num1=stack.pop();
-                       num2=stack.pop();
-                    //    System.out.println(num1+ " " + num2);
-                          stack.push(num2/num1);
-                        break;
-                 case "*":
-                         num1=stack.pop();
-                         num2=stack.pop();
-                          stack.push(num1*num2);
-                        break;
-                default:
-                        int num=Integer.parseInt(n);
-                        // System.out.println(num);
-                        stack.push(num);
-                        break;
+        Stack<Integer> stack=new Stack<>();
+        for(String s:tokens){
+          
+            if(s.equals("+")){
+                int n1=stack.pop();
+                int n2=stack.pop();
+                stack.push(n1+n2);
+            }
+            else if(s.equals("-")){
+                int n1=stack.pop();
+                int n2=stack.pop();
+                stack.push(n2-n1);
+            }
+            else if(s.equals("*")){
+                int n1=stack.pop();
+                int n2=stack.pop();
+                stack.push(n1*n2);
+            }
+            else if(s.equals("/")){
+                int n1=stack.pop();
+                int n2=stack.pop();
+                // System.out.println(n1/n2);
+                stack.push(n2/n1);
+            }
+            else{
+                int num=Integer.parseInt(s);
+                //  System.out.println(num);
+                stack.add(num);
             }
         }
+        
         return stack.isEmpty()?0:stack.pop();
+
     }
 }

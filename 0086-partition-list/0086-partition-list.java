983373296ae8a3d85e1ba567a3ade264428d98c9
Suma-Yogaraja <suma.yogaraja@gmail.com>
2026-04-13 12:22:39 -0700

    Time: 0 ms (100%), Space: 43.7 MB (86.16%) - LeetHub

--- 0086-partition-list/0086-partition-list.java
+++ 0086-partition-list/0086-partition-list.java
@@ -10,24 +10,25 @@
  */
 class Solution {
     public ListNode partition(ListNode head, int x) {
-        ListNode list1=new ListNode(0);
-        ListNode head1=list1;
-        ListNode list2=new ListNode(0);
-        ListNode head2=list2;
-        while(head!=null){
-            if(head.val<x){
-                list1.next=head;
-                list1=list1.next;
-            }
-            else{
-                list2.next=head;
-                list2=list2.next;
-                // System.out.println(head.val);
-            }
-            head=head.next;
+       //create twol list ,one list has nodes<x ,other has node>=x
+       ListNode list1=new ListNode(0);
+       ListNode head1=list1;
+       ListNode list2=new ListNode(0);
+       ListNode head2=list2;
+       while(head!=null){
+        if(head.val<x){
+            list1.next=head;
+            list1=list1.next;
         }
-        list2.next=null;
-         list1.next=head2.next;
-         return head1.next;
+        else{
+            list2.next=head;
+            list2=list2.next;
+        }
+        head=head.next;
+       }
+    //    System.out.println(head1.val);
+       list2.next=null;
+       list1.next=head2.next;
+       return head1.next;
     }
 }
